024ab4a3f85400afdbc2de9e26bfed99262c4dbf
Elisabeth Netland <elisabeth.netland@capgemini.com>
2021-06-16 18:10:59 +0200

    added content to landingpage + scroller

---
+++ gi-en-jul/ClientApp/src/components/Companies.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+const Companies = () => (
+    <div id='companies'>
+        <h3>For Bedrifter</h3>
+     </div>
+)
+
+export default (Companies);
---
+++ gi-en-jul/ClientApp/src/components/How.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+const How = () => (
+    <div id='how'>
+        <h3>Hvordan fungerer gi en jul</h3>
+    </div>
+)
+
+export default (How);
