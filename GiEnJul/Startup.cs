f433e64c38c5b2aff3ad373c1a3e2f857ee44adb
Martin Sommerli <martin.sommerli@capgemini.com>
2021-06-17 10:25:32 +0200

    Refactor project names

--- GiEnJul/Startup.cs
+++ GiEnJul/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace gi_en_jul
+namespace GiEnJul
 {
     public class Startup
     {
