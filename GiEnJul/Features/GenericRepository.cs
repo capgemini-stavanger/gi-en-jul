790523d30cb5195b56e66260a88a347ea23c7605
Martin Sommerli <martin.sommerli@capgemini.com>
2021-06-18 14:38:07 +0200

    Added generic repository with Get, InsertOrReplace and Delete functions.

---
+++ GiEnJul/Features/GenericRepository.cs
@@ -0,0 +1,55 @@
+﻿using GiEnJul.Infrastructure;
+using Microsoft.Azure.Cosmos.Table;
+using System.Threading.Tasks;
+
+namespace GiEnJul.Models
+{
+    public interface IGenericRepository<T> where T : TableEntity
+    {
+        public CloudTable _table { get; }
+
+
+        Task<T> GetAsync(string rowKey, string partitionKey); 
+        Task<T> InsertOrReplaceAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+    }
+
+    public class GenericRepository<T> : IGenericRepository<T> where T : TableEntity
+    {
+        public CloudTable _table { get; private set; }
+
+        public GenericRepository(ISettings settings, string tableName)
+        {
+            var storageAccount = CloudStorageAccount.Parse(settings.TableConnectionString);
+            var tableClient = storageAccount.CreateCloudTableClient();
+
+            _table = tableClient.GetTableReference(tableName);
+        }
+
+        public async Task<T> DeleteAsync(T entity)
+        {
+            var operation = TableOperation.Delete(entity);
+            var result = await _table.ExecuteAsync(operation);
+
+            return (T)result.Result;
+
+        }
+
+        public async Task<T> GetAsync(string rowKey, string partitionKey)
+        {
+            var operation = TableOperation.Retrieve(partitionKey, rowKey);
+            var result = await _table.ExecuteAsync(operation);
+
+            return (T)result.Result;
+        }
+
+        public async Task<T> InsertOrReplaceAsync(T entity)
+        {
+            var operation = TableOperation.InsertOrReplace(entity);
+            var result = await _table.ExecuteAsync(operation);
+
+            return (T)result.Result;
+        }
+    }
+
+}
