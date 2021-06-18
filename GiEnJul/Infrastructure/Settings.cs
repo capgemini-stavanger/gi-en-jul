ca92978c4f580d1b071612e3c9b97c62b4e68e6f
Martin Sommerli <martin.sommerli@capgemini.com>
2021-06-18 14:38:07 +0200

    Added generic repository with Get, InsertOrReplace and Delete functions.

---
+++ GiEnJul/Infrastructure/Settings.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace GiEnJul.Infrastructure
+{
+    public interface ISettings
+    {
+        public string TableConnectionString { get; }
+    }
+
+    public class Settings : ISettings
+    {
+        private readonly IConfiguration _configuration;
+
+        public Settings(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string TableConnectionString => _configuration.GetValue<string>("LocalTableConnection");
+    }
+}
