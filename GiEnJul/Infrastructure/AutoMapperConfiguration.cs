364ca849577b37544a413c33df400fa327a23244
Joachim Andreassen <61537815+joffe97@users.noreply.github.com>
2021-06-21 08:54:30 +0200

    Added automapper (#34)
    Co-authored-by: Joachim Andreassen <joachim.andreassen@capgemini.com>

---
+++ GiEnJul/Infrastructure/AutoMapperConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiEnJul.Infrastructure
+{
+    public static class AutoMapperConfiguration
+    {
+        public static IMapper Initialize()
+        {
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new AutoMapperProfile());
+            });
+            return mapperConfig.CreateMapper();
+        }
+    }
+}
