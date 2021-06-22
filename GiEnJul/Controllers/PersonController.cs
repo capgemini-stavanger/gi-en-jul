82d9dab3f5dce60a9236961e9e700ea90cbc9740
Odin Bjørnebo <odin.bjornebo@capgemini.com>
2021-06-22 14:30:53 +0200

    Removed added tests for endpoint from personcontroller

--- GiEnJul/Controllers/PersonController.cs
+++ GiEnJul/Controllers/PersonController.cs
@@ -24,42 +24,32 @@ namespace GiEnJul.Controllers
         public async Task<ActionResult<Person>> Test()
         {
             var person = new Person("RecipientId", Guid.NewGuid().ToString()) { Age = 5, Wish = "Leke", Gender = Gender.Male, };
-            var giver = new Giver("idd", Guid.NewGuid().ToString()) {MaxRecievers=6, FullName="Jonny Hansen", Email="JonnyH@gmail.com", PhoneNumber=99944876 };
 
             var addedPerson = await _personRepository.InsertOrReplaceAsync(person);
-            var addedGiver = await _giverRepository.InsertOrReplaceAsync(giver);
 
-            return CreatedAtAction(nameof(addedPerson), addedPerson, addedGiver);
+            return CreatedAtAction(nameof(addedPerson), addedPerson);
         }
         
         [HttpGet("DeleteTest")]
-        public async Task<ActionResult<Giver>> Test2()
+        public async Task<ActionResult<Person>> Test2()
         {
             var person = new Person("RecipientId", Guid.NewGuid().ToString()) { Age = 5, Wish = "Leke", Gender = Gender.Male, };
 
             var addedPerson = await _personRepository.InsertOrReplaceAsync(person);
             var deleted = await _personRepository.DeleteAsync(addedPerson);
             
-            var giver1 = new Giver("id1", Guid.NewGuid().ToString()) { MaxRecievers = 6, FullName = "Jonny Hansen2", Email = "JonnyH@gmail.com", PhoneNumber = 99944876 };
-            
-            var addedGiver1 = await _giverRepository.InsertOrReplaceAsync(giver1);
-            var deleted_giver = await _giverRepository.DeleteAsync(giver1);
-
-            return Ok(deleted_giver);
+            return Ok(deleted);
         }
 
         [HttpGet("GetTest")]
-        public async Task<ActionResult<Entities.Giver>> Test3()
+        public async Task<ActionResult<Entities.Person>> Test3()
         {
             var guid = "heiheihei";
             var person = new Person("RecipientId", guid) { Age = 5, Wish = "Leke", Gender = Gender.Male, };
-            var guid2 = "ThisIsID";
-            var giver2 = new Giver("Stav", guid2) { MaxRecievers = 3, FullName = "Benny Johnson", Email = "Benn@gmail.com", PhoneNumber = 99109222 };
 
             await _personRepository.InsertOrReplaceAsync(person);
-            await _giverRepository.InsertOrReplaceAsync(giver2);
 
-            return await _giverRepository.GetAsync("ThisIsID", guid2);
+            return await _personRepository.GetAsync("RecipientId", guid);
         }
     }
 }
