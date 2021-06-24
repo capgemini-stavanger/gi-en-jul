fb7d2189f75e8c47f72b9a19e6c70b1967910a35
Odin Bjørnebo <odin.bjornebo@capgemini.com>
2021-06-24 13:03:00 +0200

    added repository for connection

--- GiEnJul/Infrastructure/AutoMapperProfile.cs
+++ GiEnJul/Infrastructure/AutoMapperProfile.cs
@@ -11,6 +11,26 @@ namespace GiEnJul.Infrastructure
             CreateMap<Models.Person, Entities.Person>();
             CreateMap<Entities.Giver, Models.Giver>();
             CreateMap<Models.Giver, Entities.Giver>();
+            CreateMap<Models.Giver,Entities.Connection >();
+            CreateMap<Models.Recipient, Entities.Connection>();
+            CreateMap<Models.Giver, Entities.Connection>()
+                .ForMember(dest => dest.GiverEmail, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.SubmitterEmail, opt => opt.MapFrom(src => src.Receiver.ContactEmail))
+                .ForMember(dest => dest.GiverFullName, opt => opt.MapFrom(src => src.FullName))
+                .ForMember(dest => dest.GiverLocation, opt => opt.MapFrom(src => src.Location))
+                .ForMember(dest => dest.GiverPhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
+                .ForMember(dest => dest.ReceiverLocation, opt => opt.MapFrom(src => src.Receiver.Location))
+                .ForMember(dest => dest.SubmitterFullName, opt => opt.MapFrom(src => src.Receiver.ContactFullName))
+                .ForMember(dest => dest.PersonCount, opt => opt.MapFrom(src => src.Receiver.FamilyMembers.Count));
+            CreateMap<Models.Recipient, Entities.Connection>()
+                .ForMember(dest => dest.GiverEmail, opt => opt.MapFrom(src => src.Giver.Email))
+                .ForMember(dest => dest.SubmitterEmail, opt => opt.MapFrom(src => src.ContactEmail))
+                .ForMember(dest => dest.GiverFullName, opt => opt.MapFrom(src => src.Giver.FullName))
+                .ForMember(dest => dest.GiverLocation, opt => opt.MapFrom(src => src.Giver.Location))
+                .ForMember(dest => dest.GiverPhoneNumber, opt => opt.MapFrom(src => src.Giver.PhoneNumber))
+                .ForMember(dest => dest.ReceiverLocation, opt => opt.MapFrom(src => src.Location))
+                .ForMember(dest => dest.SubmitterFullName, opt => opt.MapFrom(src => src.ContactFullName))
+                .ForMember(dest => dest.PersonCount, opt => opt.MapFrom(src => src.FamilyMembers.Count));
         }
     }
 }
