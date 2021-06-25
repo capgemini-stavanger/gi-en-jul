031cdd170075ff5581f661635c9dfe0d556f431d
Elisabeth Netland <elisabeth.netland@capgemini.com>
2021-06-25 12:39:27 +0200

    Added an error controller thatsends an RFC 7807-compliant payload to the client.

---
+++ GiEnJul/Controllers/ErrorController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace GiEnJul.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [Route("/error-local-development")]
+        public IActionResult ErrorLocalDevelopment([FromServices] IWebHostEnvironment webHostEnvironment)
+        {
+            
+            if (webHostEnvironment.EnvironmentName != "Development")
+            {
+                throw new InvalidOperationException(
+                    "This shouldn't be invoked in non-development environments.");
+            }
+
+            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            return Problem(
+                detail: context.Error.StackTrace,
+                title: context.Error.Message);
+        }
+
+        [Route("/error")]
+        public IActionResult Error() => Problem();
+    }
+}
