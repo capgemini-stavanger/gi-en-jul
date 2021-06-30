f616b63eea890fcf0a5d497f936a3707f05afa5f
Joachim Andreassen <joachim.andreassen@capgemini.com>
2021-06-30 08:34:33 +0200

    Added more data annotations to the recipient model, along with tests

--- GiEnJul.Test/ModelTests/RecipientTest.cs
+++ GiEnJul.Test/ModelTests/RecipientTest.cs
@@ -15,6 +15,7 @@ namespace GiEnJul.Test.ModelTests
 
         public static IEnumerable<object[]> GetInvalidRecipients()
         {
+            yield return new object[] { new Models.Recipient("Stavanger") };
             yield return new object[] { new Models.Recipient("") {
                 ContactEmail = "o@v.no",
                 ContactFullName = "daniel",
@@ -32,6 +33,146 @@ namespace GiEnJul.Test.ModelTests
                     new Models.Person("Stavanger") { Wish = "sko", Age = 6, Gender = Models.Gender.Female }
                 }
             }}; //Missing location
+            yield return new object[] { new Models.Recipient("Stavanger") {
+                ContactEmail = "o@v.no",
+                ContactFullName = "daniel",
+                ContactPhoneNumber = "99944999",
+                Institution = "NAV",
+                ReferenceId = "2",
+                Dessert = "sjokoladepudding",
+                Note = "",
+                Event = "JUL2021",
+                FamilyMembers =
+                {
+                    new Models.Person("Stavanger") { Wish = "leke", Age = 4, Gender = Models.Gender.Female },
+                    new Models.Person("Stavanger") { Age = 47, Gender = Models.Gender.Male },
+                    new Models.Person("Stavanger") { Wish = "sko", Age = 6, Gender = Models.Gender.Female }
+                }
+            }}; // Missing dinner
+            yield return new object[] { new Models.Recipient("Stavanger") {
+                ContactEmail = "o@v.no",
+                ContactFullName = "daniel",
+                ContactPhoneNumber = "99944999",
+                Institution = "NAV",
+                ReferenceId = "2",
+                Dinner = "pinnekjøtt",
+                Note = "",
+                Event = "JUL2021",
+                FamilyMembers =
+                {
+                    new Models.Person("Stavanger") { Wish = "leke", Age = 4, Gender = Models.Gender.Female },
+                    new Models.Person("Stavanger") { Age = 47, Gender = Models.Gender.Male },
+                    new Models.Person("Stavanger") { Wish = "sko", Age = 6, Gender = Models.Gender.Female }
+                }
+            }}; // Missing dessert
+            yield return new object[] { new Models.Recipient("Stavanger") {
+                ContactEmail = "o@v.no",
+                ContactFullName = "daniel",
+                ContactPhoneNumber = "99944999",
+                Institution = "NAV",
+                ReferenceId = "2",
+                Dinner = "pinnekjøtt",
+                Dessert = "sjokoladepudding",
+                Note = "",
+                FamilyMembers =
+                {
+                    new Models.Person("Stavanger") { Wish = "leke", Age = 4, Gender = Models.Gender.Female },
+                    new Models.Person("Stavanger") { Age = 47, Gender = Models.Gender.Male },
+                    new Models.Person("Stavanger") { Wish = "sko", Age = 6, Gender = Models.Gender.Female }
+                }
+            }}; // Missing event
+            yield return new object[] { new Models.Recipient("Stavanger") {
+                ContactEmail = "o@v.no",
+                ContactPhoneNumber = "99944999",
+                Institution = "NAV",
+                ReferenceId = "2",
+                Dinner = "pinnekjøtt",
+                Dessert = "sjokoladepudding",
+                Note = "",
+                Event = "JUL2021",
+                FamilyMembers =
+                {
+                    new Models.Person("Stavanger") { Wish = "leke", Age = 4, Gender = Models.Gender.Female },
+                    new Models.Person("Stavanger") { Age = 47, Gender = Models.Gender.Male },
+                    new Models.Person("Stavanger") { Wish = "sko", Age = 6, Gender = Models.Gender.Female }
+                }
+            }}; // Missing contact full name
+            yield return new object[] { new Models.Recipient("Stavanger") {
+                ContactEmail = "feil",
+                ContactFullName = "daniel",
+                ContactPhoneNumber = "99944999",
+                Institution = "NAV",
+                ReferenceId = "2",
+                Dinner = "pinnekjøtt",
+                Dessert = "sjokoladepudding",
+                Note = "",
+                Event = "JUL2021",
+                FamilyMembers =
+                {
+                    new Models.Person("Stavanger") { Wish = "leke", Age = 4, Gender = Models.Gender.Female },
+                    new Models.Person("Stavanger") { Age = 47, Gender = Models.Gender.Male },
+                    new Models.Person("Stavanger") { Wish = "sko", Age = 6, Gender = Models.Gender.Female }
+                }
+            }}; // Invalid contact email
+            yield return new object[] { new Models.Recipient("Stavanger") {
+                ContactEmail = "o@v.no",
+                ContactFullName = "daniel",
+                ContactPhoneNumber = "abc",
+                Institution = "NAV",
+                ReferenceId = "2",
+                Dinner = "pinnekjøtt",
+                Dessert = "sjokoladepudding",
+                Note = "",
+                Event = "JUL2021",
+                FamilyMembers =
+                {
+                    new Models.Person("Stavanger") { Wish = "leke", Age = 4, Gender = Models.Gender.Female },
+                    new Models.Person("Stavanger") { Age = 47, Gender = Models.Gender.Male },
+                    new Models.Person("Stavanger") { Wish = "sko", Age = 6, Gender = Models.Gender.Female }
+                }
+            }}; // Invalid contact phone number
+            yield return new object[] { new Models.Recipient("Stavanger") {
+                ContactEmail = "o@v.no",
+                ContactFullName = "daniel",
+                ContactPhoneNumber = "99944999",
+                ReferenceId = "2",
+                Dinner = "pinnekjøtt",
+                Dessert = "sjokoladepudding",
+                Note = "",
+                Event = "JUL2021",
+                FamilyMembers =
+                {
+                    new Models.Person("Stavanger") { Wish = "leke", Age = 4, Gender = Models.Gender.Female },
+                    new Models.Person("Stavanger") { Age = 47, Gender = Models.Gender.Male },
+                    new Models.Person("Stavanger") { Wish = "sko", Age = 6, Gender = Models.Gender.Female }
+                }
+            }}; // Missing institution
+            yield return new object[] { new Models.Recipient("Stavanger") {
+                ContactEmail = "o@v.no",
+                ContactFullName = "daniel",
+                ContactPhoneNumber = "99944999",
+                Institution = "NAV",
+                ReferenceId = "2",
+                Dinner = "pinnekjøtt",
+                Dessert = "sjokoladepudding",
+                Note = "",
+                Event = "JUL2021",
+                FamilyMembers = {}
+            }}; // No familyMembers
+            var fm1000 = new List<Models.Person>();
+            for (var i = 1; i <= 1000; i++) fm1000.Add(new Models.Person("Stavanger"));
+            yield return new object[] { new Models.Recipient("Stavanger") {
+                ContactEmail = "o@v.no",
+                ContactFullName = "daniel",
+                ContactPhoneNumber = "99944999",
+                Institution = "NAV",
+                ReferenceId = "2",
+                Dinner = "pinnekjøtt",
+                Dessert = "sjokoladepudding",
+                Note = "",
+                Event = "JUL2021",
+                FamilyMembers = fm1000
+            }}; // Too many familyMembers
         }
 
         [Theory]
--- GiEnJul/Models/Recipient.cs
+++ GiEnJul/Models/Recipient.cs
@@ -14,19 +14,31 @@ namespace GiEnJul.Models
             Location = location;
         }
 
+        [Required]
         public string Dinner { get; set; }
+        [Required]
         public string Dessert { get; set; }
         public string Note { get; set; }
+        [Required]
         public string Event { get; set; }
         [Required]
         public string Location { get; set; }
         
+        [Required]
         public string ContactFullName { get; set; }
+        [Required]
+        [EmailAddress]
         public string ContactEmail { get; set; }
+        [Required]
+        [Phone]
         public string ContactPhoneNumber { get; set; }
+        [Required]
         public string Institution { get; set; }
         public string ReferenceId { get; set; }
 
+        [Required]
+        [MinLength(1)]
+        [MaxLength(999)]
         public List<Person> FamilyMembers { get; set; } = new List<Person>();
         public Giver Giver { get; set; }
     }
