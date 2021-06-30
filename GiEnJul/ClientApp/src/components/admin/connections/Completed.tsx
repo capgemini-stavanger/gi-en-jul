b33a7be8c3720388bb3a6b7f3421ac7076bceede
Odin Bjørnebo <odin.bjornebo@capgemini.com>
2021-06-30 12:14:54 +0200

    added connections component and micro component

--- GiEnJul/ClientApp/src/components/admin/connections/Completed.tsx
+++ GiEnJul/ClientApp/src/components/admin/connections/Completed.tsx
@@ -1,11 +1,104 @@
+import { Accordion, AccordionDetails, AccordionSummary, createStyles, Grid, makeStyles, Theme, Typography } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import * as React from 'react';
+import Circle from '../common/Circle';
+import { Box } from '@material-ui/core';
+import ConnectionsField from './ConnectionsField';
 
-function Completed() {
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles
+    ({
+        root: {
+            width: '100%',
+        },
+        heading: {
+            fontSize: theme.typography.pxToRem(15),
+            flexBasis: '33.33%',
+            flexShrink: 0,
+            alignItems:"center",
+        },
+        secondaryHeading: {
+            fontSize: theme.typography.pxToRem(15),
+            color: theme.palette.text.secondary,
+        },
+        items: {
+            margin:"1em",
+        },
+        downwardsContainer: {
+            display: "flex",
+            width: "100%",
+            flexDirection: "row",
+            flexWrap: "wrap",
+        },
+        downwardHeader: {
+            display: "flex",
+            width: "100%",
+            justifyContent:"space-between",
+        },
+        headerItems: {
+            marginRight: "65%",
+            marginLeft: "2em",
+        
+        },
+        container: {
+            display: "flex",
+            width: "100%",
+        },
+        headerGrid: {
+
+        },
+    }),
+);
+
+interface ICompletedProps {
+    //Need to implement connections dict
+    // The information should come from the macrocomponent
+}
+
+const Completed: React.FC<ICompletedProps> = (props) => {
+    const classes = useStyles();
     return(
-        <div>
-            <h4 className="text-uppercase">Completed</h4>
+        <div className={classes.root}>
+            <Accordion defaultExpanded>
+                <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1c-content"
+                id="panel1c-header"
+                justify-content="space between"
+                >   
+                    <Box display="flex" className={classes.heading} align-items="center">
+                        <Typography component="h4" className={classes.items} >FULLFØRTE KOBLINGER</Typography>
+                    </Box>
+                    <Box display="flex" className={classes.heading} align-items="center">
+                        <Circle prop1={"green"}/>
+                        <Typography className={classes.items}>Bekreftet</Typography>
+                    </Box>
+                    <Box display="flex" className={classes.heading} align-items="center" justifyContent="flex-start">
+                        <Circle prop1={"yellow"}/>
+                        <Typography className={classes.items}>Ikke Bekreftet</Typography>
+                    </Box>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Box className={classes.downwardsContainer}>
+                        <Box className={classes.container} justifyContent="flex-end">
+                            <Grid container spacing={2}>
+                                <Grid item xs>
+                                    <Typography component="h2" className={classes.items} >Familie</Typography>
+                                </Grid>
+                                <Grid item xs></Grid>
+                                <Grid item xs></Grid>
+                                <Grid item xs></Grid>
+                                <Grid item xs>    
+                                    <Typography component="h2" className={classes.items} >Giver</Typography>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                        <ConnectionsField personcount={5} recipientnumber={"NAV2ET"} giverfullname={"Johann Boi"} status={false} />
+                        <ConnectionsField personcount={3} recipientnumber={"BARNQS"} giverfullname={"Johann Boi"} status={true} />
+                    </Box>
+                </AccordionDetails>
+            </Accordion>
         </div>
     );
 }
-
 export default Completed;
---
+++ GiEnJul/ClientApp/src/components/admin/connections/ConnectionsField.tsx
@@ -0,0 +1,67 @@
+import { Accordion, AccordionDetails, AccordionSummary, createStyles, makeStyles, Theme, Typography } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Box } from '@material-ui/core';
+import * as React from 'react';
+
+interface IConnectionsFieldProps {
+    personcount: number,
+    giverfullname: string,
+    recipientnumber: string,
+    status: boolean,
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles
+    ({
+        root: {
+            width: '100%',
+        },
+        heading: {
+            fontSize: theme.typography.pxToRem(15),
+            flexBasis: '50%',
+            alignItems:"center",
+            justifyContent: "flex-start",
+        },
+        secondaryHeading: {
+            fontSize: theme.typography.pxToRem(15),
+            flexBasis: '50%',
+            alignItems:"center",
+            justifyContent: "flex-end",
+        },
+        items: {
+            margin: "1em",
+        },
+    }),
+);
+
+const ConnectionsField: React.FC<IConnectionsFieldProps> = (props) => {
+
+    const classes = useStyles();
+  return(
+    <div className={classes.root}>
+        <Accordion>
+            <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1c-content"
+            id="panel1c-header">
+                <Box className={classes.heading} display="flex" >
+                    <Typography className={classes.items}>{props.recipientnumber}</Typography>
+                    <Typography className={classes.items}>Antall: {props.personcount}</Typography>
+                </Box>
+                <Box className={classes.secondaryHeading} display="flex" >
+                    <Typography className={classes.items} >{props.giverfullname}</Typography>
+                    <Typography className={classes.items}>{props.status ? "Bekreftet" : "Ikke Bekreftet"}</Typography>
+                </Box>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Box width="100%" display="flex" flex-grow="1">
+                    <div >Familie</div>
+                    <div margin-left="100px">Giver</div>
+                </Box>
+            </AccordionDetails>
+        </Accordion>
+    </div>
+    );
+}
+
+export default ConnectionsField;
