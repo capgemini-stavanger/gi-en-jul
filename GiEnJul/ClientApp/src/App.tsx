08114bf5fb53956b46eee108162805b907cea172
Odin Bjørnebo <odin.bjornebo@capgemini.com>
2021-07-02 13:33:04 +0200

    tested login and logout button

--- GiEnJul/ClientApp/src/App.tsx
+++ GiEnJul/ClientApp/src/App.tsx
@@ -1,19 +1,24 @@
 import * as React from 'react';
 import { Route } from 'react-router';
-import Layout from './components/Layout';
-import Home from './routes/home/Home';
-import Institution from './components/institution/InstitutionMacro';
 import AdminPage from '../src/components/admin/AdminPage';
+import Institution from './components/institution/InstitutionMacro';
+import Layout from './components/Layout';
+import './custom.css';
+import Home from './routes/home/Home';
 import SignUp from './routes/registerGiver/SignUp';
-
-import './custom.css'
-
+import { Auth0Provider } from "@auth0/auth0-react";
 
 export default () => (
-    <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/registrer-familie' component={ Institution } />
-        <Route path='/admin' component={ AdminPage } />
-        <Route path='/bli-giver' component={ SignUp }/>
-    </Layout>
+    <Auth0Provider
+    domain="dev-r7fmessb.eu.auth0.com"
+    clientId="sxpa9h7p3UnlhP2aIttZHlLVr41rU0RR"
+    redirectUri={window.location.origin}
+    >
+        <Layout>
+            <Route exact path='/' component={Home} />
+            <Route path='/registrer-familie' component={ Institution } />
+            <Route path='/admin' component={ AdminPage } />
+            <Route path='/bli-giver' component={ SignUp }/>
+        </Layout>
+    </Auth0Provider>
 );
---
+++ GiEnJul/ClientApp/src/components/login/LogOutButton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@material-ui/core";
+
+const LogOutButton = () => {
+    const { logout } = useAuth0();
+
+
+  return <Button variant="contained" onClick={() => logout()}>Log Out</Button>;
+};
+
+export default LogOutButton;
