e6a3952a40dfcc57ce87362c060d5ddd871246b4
elisabethnetland <elisabeth.s.netland@gmail.com>
2021-07-02 14:12:10 +0200

    commented out unused states

--- GiEnJul/ClientApp/src/components/admin/editLandingPage/EditQuestions.tsx
+++ GiEnJul/ClientApp/src/components/admin/editLandingPage/EditQuestions.tsx
@@ -7,16 +7,16 @@ import draftToHtml from 'draftjs-to-html';
 import { FormGroup } from '@material-ui/core';
 
 const EditQuestions = () => {
-    const [title, setTitle] = useState<string>('');
-    const [picture, setPicture] = useState<string>('');
+    // const [title, setTitle] = useState<string>('');
+    // const [picture, setPicture] = useState<string>('');
     const [content, setContent] = useState<string>('');
-    const [headline, setHeadline] = useState<string>('');
+    // const [headline, setHeadline] = useState<string>('');
     const [editorState, setEditorState] = useState<EditorState>(EditorState.createEmpty());
 
-    const [saving, setSaving] = useState<boolean>(false);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [success, setSuccess] = useState<string>('');
-    const [error, setError] = useState<string>('');
+    // const [saving, setSaving] = useState<boolean>(false);
+    // const [loading, setLoading] = useState<boolean>(true);
+    // const [success, setSuccess] = useState<string>('');
+    // const [error, setError] = useState<string>('');
 
     return(
         <FormGroup>
