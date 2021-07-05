3a6f47c2a850f09116021d493783dc00f6df0b0b
Sommerli <martinsommerli@gmail.com>
2021-07-05 18:01:30 +0200

    Added controller method to fetch active locations

---
+++ GiEnJul/Controllers/EventController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using GiEnJul.Features;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiEnJul.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly IEventRepository _eventRepository;
+        private readonly ILogger _log;
+        private readonly IMapper _mapper;
+
+        public EventController(IEventRepository eventRepository, ILogger log, IMapper mapper)
+        {
+            _eventRepository = eventRepository;
+            _log = log;
+            _mapper = mapper;
+        }
+
+        // GET api/Events/ActiveLocations
+        [HttpGet("ActiveLocations")]
+        public async Task<string[]> GetActiveLocationsAsync()
+        {
+            return await _eventRepository.GetLocationsWithActiveEventAsync();
+        }
+    }
+}
