9971ec6230725bd3a74d4fd82ce26c7adacc14b1
Sommerli <martinsommerli@gmail.com>
2021-07-05 18:07:43 +0200

    Removed unused imports

--- GiEnJul/Controllers/EventController.cs
+++ GiEnJul/Controllers/EventController.cs
@@ -1,11 +1,7 @@
 ﻿using AutoMapper;
 using GiEnJul.Features;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Serilog;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace GiEnJul.Controllers
