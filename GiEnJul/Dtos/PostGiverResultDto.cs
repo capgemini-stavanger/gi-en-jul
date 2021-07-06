4119d952ee059612dc5856f348d4eb7f10aaf42c
Sommerli <martinsommerli@gmail.com>
2021-07-06 10:11:29 +0200

    Remove unnecessary imports

--- GiEnJul/Dtos/PostGiverResultDto.cs
+++ GiEnJul/Dtos/PostGiverResultDto.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace GiEnJul.Dtos
+﻿namespace GiEnJul.Dtos
 {
     public class PostGiverResultDto 
     {
