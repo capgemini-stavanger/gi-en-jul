60127aaf40d7bed923d04b150a030cf51c547e65
Odin Bjørnebo <odinbjoernebo@gmail.com>
2021-07-06 12:40:06 +0200

    added option to logoutfunc

--- GiEnJul/ClientApp/src/components/admin/AdminPage.tsx
+++ GiEnJul/ClientApp/src/components/admin/AdminPage.tsx
@@ -2,7 +2,6 @@ import { withAuthenticationRequired } from "@auth0/auth0-react";
 import { Container } from "@material-ui/core";
 import * as React from "react";
 import LoadingPage from "../../common/components/LoadingPage";
-import LoginButton from "../login/LoginButton";
 import LogOutButton from "../login/LogOutButton";
 import AdminMenu from "./common/AdminMenu";
 import Completed from "./connections/Completed";
@@ -11,7 +10,6 @@ import Suggested from "./connections/Suggested";
 function AdminPage() {
   return (
     <>
-      <LoginButton></LoginButton>
       <LogOutButton></LogOutButton>
       <Container maxWidth="xl">
         <AdminMenu />
