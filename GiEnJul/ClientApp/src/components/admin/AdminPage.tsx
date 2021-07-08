d931b9c75e8b818962884c94824ee4a550a14f16
Odin <odinbjoernebo@gmail.com>
2021-07-08 16:08:23 +0200

    added errorpage and if now throws error

--- GiEnJul/ClientApp/src/components/admin/AdminPage.tsx
+++ GiEnJul/ClientApp/src/components/admin/AdminPage.tsx
@@ -3,6 +3,7 @@ import { Container } from "@material-ui/core";
 import * as React from "react";
 import { useEffect, useState } from "react";
 import LoadingPage from "../../common/components/LoadingPage";
+import ErrorPage from "../common/ErrorPage";
 import InstitutionMacro from "../institution/InstitutionMacro";
 import LogOutButton from "../login/LogOutButton";
 import AdminMenu from "./common/AdminMenu";
@@ -10,62 +11,56 @@ import Completed from "./connections/Completed";
 import Suggested from "./connections/Suggested";
 
 type UserDataType = {
-  app_metadata?: {role:string},
-  email?: string,
-  email_verified?: boolean,
-  identities?: [{}],
-  last_ip?: number,
-  last_login?: string,
-  logins_count?: number,
-  name?: string,
-  nickname?: string,
-  picture?: string,
-  updated_at?: string,
-  user_metadata?: {string:string}
-}
-
+  app_metadata?: { role: string };
+  email?: string;
+  email_verified?: boolean;
+  identities?: [{}];
+  last_ip?: number;
+  last_login?: string;
+  logins_count?: number;
+  name?: string;
+  nickname?: string;
+  picture?: string;
+  updated_at?: string;
+  user_metadata?: { string: string };
+};
 
 function AdminPage() {
   const { user, getAccessTokenSilently } = useAuth0();
-
   const [userData, setUserData] = useState<UserDataType | undefined>(undefined);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
-
-
+  // const [userData]
 
   async function getUserInformation(): Promise<UserDataType> {
-    
     const domain = process.env.REACT_APP_DEV_TENANT_AUTH0!;
 
-        const accessToken = await getAccessTokenSilently({
-          audience: `https://${domain}/api/v2/`,
-          scope: "read:current_user",
-        });
+    const accessToken = await getAccessTokenSilently({
+      audience: `https://${domain}/api/v2/`,
+      scope: "read:current_user",
+    });
 
-        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${
-          user!.sub
-        }`;
+    const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user!.sub}`;
 
-        const  response  = await fetch(userDetailsByIdUrl, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });        
-        const userInformation: UserDataType = await response.json();
-        return userInformation;
-
+    const response = await fetch(userDetailsByIdUrl, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const userInformation: UserDataType = await response.json();
+    return userInformation;
   }
   useEffect(() => {
-      getUserInformation().then((response: UserDataType) => setUserData(response)),
-      setIsLoaded(true)
-  },[]);
-
-
+     getUserInformation().then( (response: UserDataType) => {
+       setUserData(response)
+       setIsLoaded(true)
+     }
+    );
+  }, []);
 
   if (!isLoaded) {
     return <LoadingPage />;
   } else if (isLoaded && userData == null) {
-    return <LoadingPage />;
+    throw new Error("userData was not Loaded");
   } else {
     if (userData!.app_metadata!.role === "institution") {
       return <InstitutionMacro />;
@@ -74,14 +69,15 @@ function AdminPage() {
         <>
           <LogOutButton />
           <Container maxWidth="xl">
-            <AdminMenu />
-            <Suggested />
-            <Completed />
-          </Container>
+          <AdminMenu />
+          <Suggested />
+          <Completed />
+          <ErrorPage ErrorText={"Your Role has not yet been set"} ErrorCode={200}/>
+        </Container>
         </>
       );
     } else {
-      return <LoadingPage />;
+      return <ErrorPage ErrorText={"Your Role has not yet been set"} ErrorCode={200}/>;
     }
   }
 }
