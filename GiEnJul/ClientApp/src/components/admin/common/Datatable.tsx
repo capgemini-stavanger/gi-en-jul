354bb5eaf1d880049f86a6bdd9627f87200623b5
elisabethnetland <elisabeth.s.netland@gmail.com>
2021-07-08 13:38:27 +0200

    added fields to table

--- GiEnJul/ClientApp/src/components/admin/common/Datatable.tsx
+++ GiEnJul/ClientApp/src/components/admin/common/Datatable.tsx
@@ -1,35 +1,120 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
-import * as React from 'react';
+import * as React from "react";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Avatar,
+  Container,
+  createStyles,
+  Divider,
+  makeStyles,
+  Theme,
+  Typography,
+} from "@material-ui/core";
+import {
+  ExpandMore,
+  Group,
+  Phone,
+  Mail,
+  CheckRounded,
+  CloseRounded,
+} from "@material-ui/icons";
+import { green, red } from "@material-ui/core/colors";
+import { GiverType } from "../overview/Giver";
 
 type Props = {
-    data: JSON[];
-}
+  data: [GiverType] | [];
+};
 
-const Datatable: React.FC<Props> =({data}) => {
-    const columns = Object.keys(data[0]);
-    console.log(Object.values(data[0]))
-    console.log(Object.values(data[0]))
-    const rows = Object.values(data)
-    return (
-       <TableContainer>
-           <Table>
-               <TableHead>
-                   <TableRow>
-                          {columns.map(heading => <TableCell>{heading}</TableCell>)}
-                   </TableRow>
-                   </TableHead>
-                   <TableBody>
-                       {data.map(row => 
-                       <TableRow>
-                           {columns.map(column => 
-                           <TableCell>{row[column]}</TableCell>)}
-                       </TableRow>)}
-                   </TableBody>
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%",
+    },
+    heading: {
+      fontSize: theme.typography.pxToRem(15),
+      flexBasis: "33.33%",
+      flexShrink: 0,
+    },
+    secondaryHeading: {
+      fontSize: theme.typography.pxToRem(15),
+      color: theme.palette.text.secondary,
+      flexBasis: "33.33%",
+      flexShrink: 0,
+    },
+    matched: {
+      color: "#fff",
+      backgroundColor: green[500],
+    },
+    notMatched: {
+      color: "#FF0000",
+      backgroundColor: red[500],
+    },
+  })
+);
 
-               
-           </Table>
-       </TableContainer>
-    )
-}
+const Datatable: React.FC<Props> = ({ data }) => {
+  const classes = useStyles();
+
+  const formatFamily = (input: Number) => {
+    if (input === 2) {
+      return "< 3";
+    }
+    if (input === 5) {
+      return "3 - 5";
+    } else {
+      return "> 5";
+    }
+  };
+
+  const handleMatched = (input: Boolean) => {
+    if (input) {
+      return classes.matched;
+    } else {
+      return classes.notMatched;
+    }
+  };
+
+  return (
+    <Container>
+      {data.map((x) => (
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMore />}
+            aria-controls="panel1bh-content"
+            id="panel1bh-header"
+          >
+            <Typography className={classes.heading}>{x.fullName}</Typography>
+            <Typography className={classes.secondaryHeading}>
+              <Group />
+              {formatFamily(x.maxRecievers)}
+            </Typography>
+            <Typography>
+              <Avatar className={handleMatched(x.hasConfirmedMatch)}>
+                {x.hasConfirmedMatch ? (
+                  <CheckRounded style={{ color: "#FFFFFF" }} />
+                ) : (
+                  <CloseRounded style={{ color: "#FFFFFF" }} />
+                )}
+              </Avatar>
+            </Typography>
+          </AccordionSummary>
+          <Divider />
+          <AccordionDetails>
+            <Typography>
+              <Phone /> {x.phoneNumber}
+            </Typography>
+          </AccordionDetails>
+          <AccordionDetails>
+            <Typography>
+              <Mail />
+              {x.email}
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </Container>
+  );
+};
 
 export default Datatable;
--- GiEnJul/ClientApp/src/components/admin/overview/Giver.tsx
+++ GiEnJul/ClientApp/src/components/admin/overview/Giver.tsx
@@ -1,42 +1,49 @@
 import { Container } from "@material-ui/core";
 import * as React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Datatable from "../common/Datatable";
 
-const Giver = () => {
-    const [data, setData] = useState([JSON]);
-
-    useEffect(() => {
-        fetch('api/admin/allgivers', {
-            method: 'GET', // *GET, POST, PUT, DELETE, etc.
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
-        .then((response) => 
-            response.json())
-            // if (response.status === 201) {
-            //     trigger(true);
-            // }
-        .then((json) => setData(json))
-        .catch((errorStack) => {
-            console.log(errorStack);
-        })
-    }, [])
-
-    console.log(data);
-    console.log(data[1]);
-
-
-    return (
-        <Container>
-            <Datatable
-            data = {data}></Datatable>
-            
-        </Container>
-    )
-
+export interface GiverType {
+  email: string;
+  eventName: string;
+  fullName: string;
+  hasConfirmedMatch: Boolean;
+  isSuggestedMatch: Boolean;
+  location: string;
+  matchedRecipient?: string;
+  maxRecievers: Number;
+  partitionKey: string;
+  rowKey: string;
+  phoneNumber: string;
 }
 
+const Giver = () => {
+  const [data, setData] = useState<[GiverType] | []>([]);
+
+  useEffect(() => {
+    async function fetchGivers() {
+      await fetch("api/admin/allgivers", {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          return setData(json);
+        })
+        .catch((errorStack) => {
+          console.log(errorStack);
+        });
+    }
+    fetchGivers();
+  }, []);
+
+  return (
+    <Container>
+      <Datatable data={data}></Datatable>
+    </Container>
+  );
+};
 export default Giver;
