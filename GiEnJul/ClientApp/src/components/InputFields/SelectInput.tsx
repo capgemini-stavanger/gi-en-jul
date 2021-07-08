4ff0d995b0241fb170aacb6cea5f5a102ceceab5
Joachim Andreassen <joachim.andreassen@capgemini.com>
2021-07-08 10:58:38 +0200

    Started changing input and select components

--- GiEnJul/ClientApp/src/components/InputFields/SelectInput.tsx
+++ GiEnJul/ClientApp/src/components/InputFields/SelectInput.tsx
@@ -1,76 +1,104 @@
-import { FormControl, FormControlTypeMap, FormHelperText, InputLabel, MenuItem, Select } from "@material-ui/core";
+import {
+	FormControl,
+	FormControlTypeMap,
+	FormHelperText,
+	InputLabel,
+	MenuItem,
+	Select,
+} from "@material-ui/core";
 import { OverridableComponent } from "@material-ui/core/OverridableComponent";
 import React, { useState } from "react";
+import { useEffect } from "react";
 import { FC } from "react";
 import { isMobile } from "../../common/functions/IsMobile";
 
-interface ISelect extends OverridableComponent<FormControlTypeMap<{}, "div">> {
-    name: string,
-    options: {value: string | number | readonly string[] | undefined, text: string}[],
-    value: string | number | readonly string[] | undefined,
-    onChange:   ((event: React.ChangeEvent<{
-                    name?: string;
-                    value: unknown;
-                }>, child: React.ReactNode) => void),
+export interface ISelect
+	extends OverridableComponent<FormControlTypeMap<{}, "div">> {
+	name: string;
+	options: {
+		value: string | number | readonly string[] | undefined;
+		text: string;
+	}[];
+	value: string | number | readonly string[] | undefined;
+	onChange: (
+		event: React.ChangeEvent<{ name?: string; value: unknown }>,
+		child: React.ReactNode
+	) => void;
 
-    id?: string,
-    className?: string,
-    label?: string,
-    placeholder?: string,
-    autoComplete?: string,
-    errorMessage?: string,
-    error?: boolean,
-    autoFocus?: boolean
+	id?: string;
+	className?: string;
+	label?: string;
+	placeholder?: string;
+	autoComplete?: string;
+	errorMessage?: string;
+	error?: boolean;
+	autoFocus?: boolean;
 }
 
-const SelectInput: FC<ISelect> = (
-    {   
-        name,
-        options,
-        value,
-        onChange,
+const SelectInput: FC<ISelect> = ({
+	name,
+	options,
+	value,
+	onChange,
 
-        id,
-        className,
-        label,
-        placeholder,
-        autoComplete,
-        errorMessage,
-        error,
-        autoFocus,
-        ...rest
-    },
-) => {
-    const [isMob,] = useState(isMobile());
-    return (
-        <FormControl 
-            error={!!errorMessage}
-            {...rest}
-        >
-            <InputLabel htmlFor={id}>{label}</InputLabel>
-            <Select 
-                native={isMobile()}
-                error={!!error}
-                value={value}
-                onChange={onChange}
-                autoFocus={autoFocus}
-                label={label}
-                inputProps={{
-                    name: name,
-                    id: id
-                }}
-                autoComplete={autoComplete}
-                placeholder={placeholder}
-                className={className}
-            >
-            {options && options.map(o => 
-                {return isMob ? 
-                <option key={`n_${name}_${o.text}`} value={o.value} className="text-capitalize">{o.text}</option> : 
-                <MenuItem key={`${name}_${o.text}`} value={o.value} className="text-capitalize">{o.text}</MenuItem>})}
-            </Select>
-            {error && errorMessage && <FormHelperText>{errorMessage}</FormHelperText>}
-        </FormControl>
-    )
-}
+	id,
+	className,
+	label,
+	placeholder,
+	autoComplete,
+	errorMessage,
+	error,
+	autoFocus,
+	...rest
+}) => {
+	const [isMob, setIsMob] = useState(false);
+
+	useEffect(() => {
+		setIsMob(isMobile());
+	});
+
+	return (
+		<FormControl error={!!errorMessage} {...rest}>
+			<InputLabel htmlFor={id}>{label}</InputLabel>
+			<Select
+				native={isMobile()}
+				error={!!error}
+				value={value}
+				onChange={onChange}
+				autoFocus={autoFocus}
+				label={label}
+				inputProps={{
+					name: name,
+					id: id,
+				}}
+				autoComplete={autoComplete}
+				placeholder={placeholder}
+				className={className}
+			>
+				{options &&
+					options.map((o) => {
+						return isMob ? (
+							<option
+								key={`n_${name}_${o.text}`}
+								value={o.value}
+								className="text-capitalize"
+							>
+								{o.text}
+							</option>
+						) : (
+							<MenuItem
+								key={`${name}_${o.text}`}
+								value={o.value}
+								className="text-capitalize"
+							>
+								{o.text}
+							</MenuItem>
+						);
+					})}
+			</Select>
+			{error && errorMessage && <FormHelperText>{errorMessage}</FormHelperText>}
+		</FormControl>
+	);
+};
 
 export default SelectInput;
--- GiEnJul/ClientApp/src/components/InputFields/Validators/InputValidator.tsx
+++ GiEnJul/ClientApp/src/components/InputFields/Validators/InputValidator.tsx
@@ -1,174 +1,194 @@
-﻿import { TextField } from '@material-ui/core';
-import * as React from 'react';
-import { FC, useEffect, useState } from 'react';
-import SelectInput from '../SelectInput';
-
+﻿import {
+	BaseTextFieldProps,
+	FormControlTypeMap,
+	InputBaseProps,
+	TextField,
+} from "@material-ui/core";
+import { SelectInputProps } from "@material-ui/core/Select/SelectInput";
+import * as React from "react";
+import { FC, useEffect, useState } from "react";
+import SelectInput from "../SelectInput";
 
 interface InputValidatorProps {
-    // List of methods to use for validation. 
-    // Ascending prioritation.
-    validators: ((input: string) => boolean)[],
+	// List of methods to use for validation.
+	// Ascending prioritation.
+	validators: ((input: string) => boolean)[];
 
-    // List of methods that updates any setValid value.
-    // (undefined | set value function | list same length as validators prop with set value functions)
-    setIsValids?: ((isValid: boolean) => void) | ((isValid: boolean) => void)[],
+	// List of methods that updates any setValid value.
+	// (undefined | set value function | list same length as validators prop with set value functions)
+	setIsValids?: ((isValid: boolean) => void) | ((isValid: boolean) => void)[];
 
-    // List of error that should occur, depending on which validator fails. 
-    // Has no error message if undefined.
-    // Ascending prioritation.
-    // (undefined | list same length as validators prop)
-    errorMessages?: string[],   
+	// List of error that should occur, depending on which validator fails.
+	// Has no error message if undefined.
+	// Ascending prioritation.
+	// (undefined | list same length as validators prop)
+	errorMessages?: string[];
 
-    // Increment to view any validation errors. 
-    // Initialize to 0 if errors shouldn't start active.
-    viewErrorTrigger?: number,
-    
-    value: any,
-    onChange: ((e: any) => void), 
-    label: string,
-    name: string,
-    type?: string,
-    id?: string,
-    className?: string,
-    disabled?: boolean,
-    margin?: any,
-    variant?: any,
-    autoComplete?: string,
-    fullWidth?: boolean,
-    autoFocus?: boolean,
-    size?: any,
-    multiline?: boolean,
-    rowsMax?: number | string,
-    placeholder?: string,
+	// Increment to view any validation errors.
+	// Initialize to 0 if errors shouldn't start active.
+	viewErrorTrigger?: number;
 
-    options?: {value: any, text: string}[],  // Only for "type=select".
+	value: any;
+	onChange: any;
+	label: string;
+	name: string;
+	type?: string;
+	id?: string;
+	className?: string;
+	disabled?: boolean;
+	margin?: any;
+	variant?: "standard" | "outlined" | "filled";
+	autoComplete?: string;
+	fullWidth?: boolean;
+	autoFocus?: boolean;
+	size?: any;
+	multiline?: boolean;
+	rowsMax?: number | string;
+	placeholder?: string;
+
+	options?: { value: any; text: string }[]; // Only for "type=select".
 }
 
-const InputValidator: FC<InputValidatorProps> = (
-    {   setIsValids, 
-        validators,
-        errorMessages,
-        viewErrorTrigger,
-        value, 
-        onChange, 
-        type, 
-        label,
-        name, 
-        id, 
-        className, 
-        disabled,
-        margin,
-        variant,
-        autoComplete,
-        fullWidth,
-        autoFocus,
-        size,
-        multiline,
-        rowsMax,
-        placeholder,
-        options,
-     },
-) => {
-    const [isError, setIsError] = useState(false);
-    const [errorMessage, setErrorMessage] = useState<string | undefined>("");
-    const [viewError, setViewError] = useState(false);
+const initState: {
+	isError: boolean;
+	errorMessage: string | undefined;
+	viewError: boolean;
+} = {
+	isError: false,
+	errorMessage: "",
+	viewError: false,
+};
 
-    const checkArgs = () => {
-        let validatorCount = validators.length;
-        function isInvalidOptionalArg(arg?: Array<any> | Function) {
-            return typeof arg === "object" && arg.length !== validatorCount;
-        }
-        if (isInvalidOptionalArg(setIsValids) || 
-            isInvalidOptionalArg(errorMessages) ||
-            (type === "select" && (!options || !name))) {
-            throw Error("An argument in InputValidator is invalid.");
-        }
-    }
+const InputValidator: FC<InputValidatorProps> = ({
+	setIsValids,
+	validators,
+	errorMessages,
+	viewErrorTrigger,
+	value,
+	onChange,
+	type,
+	label,
+	name,
+	id,
+	className,
+	disabled,
+	margin,
+	variant,
+	autoComplete,
+	fullWidth,
+	autoFocus,
+	size,
+	multiline,
+	rowsMax,
+	placeholder,
+	options,
+}) => {
+	const [state, setState] = useState(initState);
 
-    const validate = () => {
-        let errorMsg: string | undefined = undefined;
-        let isAnyError = false;
-        for (let i = 0; i < validators.length; i++) {
-            let isValid = validators[i](value);
-            switch (typeof setIsValids) {
-                case "object":
-                    setIsValids[i](isValid);
-                    break;
-                case "function":
-                    setIsValids(isValid);
-                    break;
-            } 
-            if (!isValid && viewError) {
-                errorMsg = errorMessages !== undefined ? errorMessages[i] : undefined;
-                isAnyError = true;
-            }
-        }
-        setIsError(isAnyError);
-        setErrorMessage(errorMsg);
-    }
+	const checkArgs = () => {
+		let validatorCount = validators.length;
+		function isInvalidOptionalArg(arg?: Array<any> | Function) {
+			return typeof arg === "object" && arg.length !== validatorCount;
+		}
+		if (
+			isInvalidOptionalArg(setIsValids) ||
+			isInvalidOptionalArg(errorMessages) ||
+			(type === "select" && (!options || !name))
+		) {
+			throw Error("An argument in InputValidator is invalid.");
+		}
+	};
 
-    const extendedOnChange = (e: any) => {
-        onChange(e);
-        setViewError(false);
-    }
+	const validate = () => {
+		let errorMsg: string | undefined = undefined;
+		let isAnyError = false;
+		for (let i = 0; i < validators.length; i++) {
+			let isValid = validators[i](value);
+			switch (typeof setIsValids) {
+				case "object":
+					setIsValids[i](isValid);
+					break;
+				case "function":
+					setIsValids(isValid);
+					break;
+			}
+			if (!isValid && state.viewError) {
+				errorMsg = errorMessages !== undefined ? errorMessages[i] : undefined;
+				isAnyError = true;
+			}
+		}
+		setState((prev) => {
+			return { ...prev, isError: isAnyError, errorMessage: errorMsg };
+		});
+	};
 
-    useEffect(() => {
-        if (viewErrorTrigger || viewErrorTrigger === undefined) setViewError(true);
-    }, [viewErrorTrigger])
+	const extendedOnChange = (e: any) => {
+		onChange(e);
+		setState((prev) => {
+			return { ...prev, viewError: false };
+		});
+	};
 
-    useEffect(() => {
-        checkArgs();
-        validate();
-    })
+	useEffect(() => {
+		if (viewErrorTrigger || viewErrorTrigger === undefined)
+			setState((prev) => {
+				return { ...prev, viewError: true };
+			});
+	}, [viewErrorTrigger]);
 
-    switch (type) {
-        case "select":
-            if (!options) throw Error("Options must be defined in select input");
-            return (
-                <SelectInput
-                    name={name}
-                    options={options}
-                    value={value}
-                    onChange={onChange}
-                    variant={variant}
-                    disabled={disabled}
-                    fullWidth={fullWidth}
-                    margin={margin}
-                    id={id}
-                    className={className}
-                    label={label}
-                    placeholder={placeholder}
-                    autoComplete={autoComplete}
-                    errorMessage={errorMessage}
-                    error={isError}
-                    autoFocus={autoFocus}
-                />
-            )
-        default:
-            return (
-                <TextField
-                    error={isError}
-                    helperText={errorMessage}
-                    label={label}
-                    type={type} 
-                    name={name} 
-                    value={value} 
-                    onChange={extendedOnChange} 
-                    id={id} 
-                    className={className} 
-                    disabled={disabled}
-                    margin={margin}
-                    variant={variant ? variant : "outlined"}
-                    autoComplete={autoComplete}
-                    fullWidth={fullWidth}
-                    autoFocus={autoFocus}
-                    size={size}
-                    multiline={multiline}
-                    rowsMax={rowsMax}
-                    placeholder={placeholder}
-                />
-            )
-    }
-}
+	useEffect(() => {
+		checkArgs();
+		validate();
+	});
+
+	switch (type) {
+		case "select":
+			if (!options) throw Error("Options must be defined in select input");
+			return (
+				<SelectInput
+					name={name}
+					options={options}
+					value={value}
+					onChange={onChange}
+					variant={variant}
+					disabled={disabled}
+					fullWidth={fullWidth}
+					margin={margin}
+					id={id}
+					className={className}
+					label={label}
+					placeholder={placeholder}
+					autoComplete={autoComplete}
+					errorMessage={state.errorMessage}
+					error={state.isError}
+					autoFocus={autoFocus}
+				/>
+			);
+		default:
+			return (
+				<TextField
+					error={state.isError}
+					helperText={state.errorMessage}
+					label={label}
+					type={type}
+					name={name}
+					value={value}
+					onChange={extendedOnChange}
+					id={id}
+					className={className}
+					disabled={disabled}
+					margin={margin}
+					variant={variant ? variant : "outlined"}
+					autoComplete={autoComplete}
+					fullWidth={fullWidth}
+					autoFocus={autoFocus}
+					size={size}
+					multiline={multiline}
+					rowsMax={rowsMax}
+					placeholder={placeholder}
+				/>
+			);
+	}
+};
 
 export default InputValidator;
