e290607e1bef412f5e3e8ca8f6aa4289ea666deb
elisabethnetland <37541100+elisabethnetland@users.noreply.github.com>
2021-07-09 13:03:05 +0200

    Admincontroller & datatable of givers (#190)
    * Get Recipients for location where IsMatched is false
    
    * added giverlist component
    
    * fetch data from backend
    
    * changed formatting of giver table
    
    * added fields to table
    
    * Get Recipients for location where IsMatched is false
    
    * added giverlist component
    
    * fetch data from backend
    
    * changed formatting of giver table
    
    * added fields to table
    
    * Changed structure in admin controller and moved styles to separate file
    
    * implemented suggestions from sommerli
    
    * changed paths in controller for better use of rest api
    
    * changed routing
    
    * commented out unused code
    
    Co-authored-by: Sommerli <martinsommerli@gmail.com>

---
+++ GiEnJul/ClientApp/src/components/admin/overview/Giver.tsx
@@ -0,0 +1,46 @@
+import { Container } from "@material-ui/core";
+import * as React from "react";
+import { useEffect, useState } from "react";
+import Datatable from "../common/Datatable";
+
+export interface GiverType {
+  email: string;
+  eventName: string;
+  fullName: string;
+  hasConfirmedMatch: Boolean;
+  isSuggestedMatch: Boolean;
+  location: string;
+  matchedRecipient?: string;
+  maxRecievers: Number;
+  partitionKey: string;
+  rowKey: string;
+  phoneNumber: string;
+}
+
+const Giver = () => {
+  const [data, setData] = useState<[GiverType] | []>([]);
+
+  useEffect(() => {
+    async function fetchGivers() {
+      await fetch("api/admin/givers", {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => setData(json))
+        .catch((errorStack) => {
+          console.log(errorStack);
+        });
+    }
+    fetchGivers();
+  }, []);
+
+  return (
+    <Container>
+      <Datatable data={data}></Datatable>
+    </Container>
+  );
+};
+export default Giver;
