937864c82f9d0ddc89a7d52879e41be73658d675
Odin <odinbjoernebo@gmail.com>
2021-07-09 12:31:36 +0200

    first draft of hook

--- GiEnJul/ClientApp/src/hooks/useApi.tsx
+++ GiEnJul/ClientApp/src/hooks/useApi.tsx
@@ -1,3 +1,40 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
-//Here will the finished custom hook go
+const backend = window.location.origin + "/api";
+
+function useApi() {
+  //Could fit multiple hooks for different purposes
+
+  /**Function to fetch data from the rest API
+   *@param url, Designates what to GET from the DB
+   * -> f.ex. givers or recipients
+   **/
+  const useFetch = <T extends any>(url: string) => {
+    const [response, setResponse] = useState<T>();
+    const [error, setError] = useState(null);
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const res = await fetch(`${backend}/${url}`);
+          if (!res.ok) {
+            throw new Error(`${res.status} ${res.statusText}`);
+          }
+          const json = await res.json();
+          setResponse(json);
+        } catch (error) {
+          setError(error);
+        }
+      };
+      fetchData();
+    }, []);
+    return { response, error };
+  };
+  return {
+    useFetch,
+  };
+
+  //More functions here
+}
+
+export default useApi;
