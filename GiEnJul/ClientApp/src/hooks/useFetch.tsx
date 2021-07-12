8cf8e38fd3d5d48ea60b13b422b2e5aeed984036
Odin Bjørnebo <odinbjoernebo@gmail.com>
2021-07-12 13:57:20 +0200

    finalized useFetch method

--- GiEnJul/ClientApp/src/hooks/useFetch.tsx
+++ GiEnJul/ClientApp/src/hooks/useFetch.tsx
@@ -1,21 +1,19 @@
-import React from "react";
 import { useEffect, useState } from "react";
 
 const backend = window.location.origin + "/api";
 
-
-const useFetch = <T extends any>(url: string, options: RequestInit) => {
+/**
+ * Returns a response from the backend as a json.
+ * @param url which controller to access and what to fetch "/admin/givers" etc.
+ * @param reqOptions where to add headers, {method: GET, ...} etc
+ */
+const useFetch = <T extends any>(url: string, reqOptions: RequestInit) => {
   const [response, setResponse] = useState<T>();
   const [error, setError] = useState(null);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch(`${backend}/admin/${url}`,{
-            method: "GET", // *GET, POST, PUT, DELETE, etc.
-            headers: {
-              "Content-Type": "application/json",
-            },
-        });
+        const res = await fetch(`${backend}/${url}`, reqOptions);
         if (!res.ok) {
           throw new Error(`${res.status}${res.statusText}`);
         }
