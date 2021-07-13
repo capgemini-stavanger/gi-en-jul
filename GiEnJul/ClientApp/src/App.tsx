bd004b175038d8b34fd940cddc29110b228a0dab
Joachim Andreassen <joachim.andreassen@capgemini.com>
2021-07-13 14:40:30 +0200

    Added missing files

--- GiEnJul/ClientApp/src/App.tsx
+++ GiEnJul/ClientApp/src/App.tsx
@@ -7,7 +7,7 @@ import NavMenuAdmin from "./common/components/NavMenuAdmin";
 import Edit from "./components/admin/editLandingPage/EditMacro";
 import Home from "./components/home/Home";
 import Institution from "./components/institution/InstitutionMacro";
-import SignUp from "./components/registerGiver/Macro";
+import RegistrationMacro from "./components/registerGiver/Macro";
 import "./custom.css";
 
 const App = () => {
@@ -18,7 +18,7 @@ const App = () => {
         <Route path="/admin" component={AdminPage} />
         <Route exact path="/" component={Home} />
         <Route path="/registrer-familie" component={Institution} />
-        <Route path="/bli-giver" component={SignUp} />
+        <Route path="/bli-giver" component={RegistrationMacro} />
         <Route path="/rediger" component={Edit} />
         <Route path="/loading" component={LoadingPage} />
       </Container>
