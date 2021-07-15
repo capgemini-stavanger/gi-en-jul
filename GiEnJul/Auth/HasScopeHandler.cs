4daeace7f98c67217c2ba48ea56a87d99257cef6
Odin Bjørnebo <odinbjoernebo@gmail.com>
2021-07-15 10:10:56 +0200

    added app settings and fixed scope files

--- GiEnJul/Auth/HasScopeHandler.cs
+++ GiEnJul/Auth/HasScopeHandler.cs
@@ -3,3 +3,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 
+namespace GiEnJul
+{
+    public class HasScopeHandler : AuthorizationHandler<HasScopeRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HasScopeRequirement requirement)
+        {
+            if (!context.User.HasClaim(c => c.Type == "scope" && c.Issuer == requirement.Issuer))
+                return Task.CompletedTask;
+            var scopes = context.User.FindFirst(c => c.Type == "scope" && c.Issuer == requirement.Issuer).Value.Split(' ');
+            if (scopes.Any(s => s == requirement.Scope))
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- GiEnJul/Auth/HasScopeRequirement.cs
+++ GiEnJul/Auth/HasScopeRequirement.cs
@@ -3,12 +3,12 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace GiEnJul
 {
-    public class HasScopeRequirment : IAuthorizationRequirement
+    public class HasScopeRequirement : IAuthorizationRequirement
     {
         public string Issuer { get; }
         public string Scope { get; }
 
-        public HasScopeRequirment(string scope, string issuer)
+        public HasScopeRequirement(string scope, string issuer)
         {
             Issuer = issuer ?? throw new ArgumentNullException(nameof(issuer));
             Scope = scope ?? throw new ArgumentNullException(nameof(scope));
