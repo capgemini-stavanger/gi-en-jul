4866c9c2930e78c63026ffc0f7ddabc0574c76c9
Sommerli <martinsommerli@gmail.com>
2021-07-15 15:31:46 +0200

    Formatting

--- GiEnJul/Clients/EmailClient.cs
+++ GiEnJul/Clients/EmailClient.cs
@@ -9,47 +9,47 @@ using Serilog;
 namespace GiEnJul.Clients
 {
     public interface IEmailClient
-	{
-		Task SendEmailAsync(string toMail, string toName, string subject, string html);
-	}
+    {
+        Task SendEmailAsync(string toMail, string toName, string subject, string html);
+    }
 
-	public class EmailClient : IEmailClient
-	{
-		private MailSettings _mailSettings { get; }
-		private readonly IWebHostEnvironment _env;
-		private readonly ILogger _log;
+    public class EmailClient : IEmailClient
+    {
+        private MailSettings _mailSettings { get; }
+        private readonly IWebHostEnvironment _env;
+        private readonly ILogger _log;
 
         public EmailClient(ISettings settings, IWebHostEnvironment env, ILogger log)
         {
-			_mailSettings = settings.MailSettings;
-			_env = env;
-			_log = log;
+            _mailSettings = settings.MailSettings;
+            _env = env;
+            _log = log;
         }
 
         public async Task SendEmailAsync(string toMail, string toName, string subject, string body)
-		{
-			var message = new MimeMessage();
-			message.From.Add(new MailboxAddress(_mailSettings.DisplayName, _mailSettings.Mail));
-			message.To.Add(new MailboxAddress(toName, toMail));
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(_mailSettings.DisplayName, _mailSettings.Mail));
+            message.To.Add(new MailboxAddress(toName, toMail));
 
-			message.Subject = _env.IsDevelopment() ? "DEVELOPMENT - " + subject : subject;
+            message.Subject = _env.IsDevelopment() ? "DEVELOPMENT - " + subject : subject;
 
-			message.Body = new TextPart("html")
-			{
-				Text = body
-			};
+            message.Body = new TextPart("html")
+            {
+                Text = body
+            };
 
-			using (var client = new SmtpClient())
-			{
-				client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+            using (var client = new SmtpClient())
+            {
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
 
-				await client.ConnectAsync(_mailSettings.Host, _mailSettings.Port);
+                await client.ConnectAsync(_mailSettings.Host, _mailSettings.Port);
 
-				await client.AuthenticateAsync(_mailSettings.Mail, _mailSettings.Password);
-				await client.SendAsync(message);
-				_log.Debug($"Sent mail to {toMail} with subject {subject}");
-				await client.DisconnectAsync(true);
-			}
-		}
-	}
+                await client.AuthenticateAsync(_mailSettings.Mail, _mailSettings.Password);
+                await client.SendAsync(message);
+                _log.Debug($"Sent mail to {toMail} with subject {subject}");
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
 }
