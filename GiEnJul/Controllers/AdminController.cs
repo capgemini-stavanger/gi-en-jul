5e80fca8258d6b0ddb00556a4b5eb03c4cae50de
Odin Bjørnebo <odinbjoernebo@gmail.com>
2021-07-22 14:58:10 +0200

    removed dup import and renamed .tsx to ts

--- GiEnJul/Controllers/AdminController.cs
+++ GiEnJul/Controllers/AdminController.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using GiEnJul.Clients;
 using GiEnJul.Models;
-using GiEnJul.Dtos;
 using Microsoft.AspNetCore.Authorization;
 
 namespace GiEnJul.Controllers
