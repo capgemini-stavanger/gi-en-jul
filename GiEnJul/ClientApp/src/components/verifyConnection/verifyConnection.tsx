80cbd586b89b80a05b6900307e2d2bfb2076f9cd
Sommerli <martinsommerli@gmail.com>
2021-07-22 10:16:20 +0200

    Added component for givers that verify their assigned connection

---
+++ GiEnJul/ClientApp/src/components/verifyConnection/verifyConnection.tsx
@@ -0,0 +1,41 @@
+﻿import * as React from "react";
+import { useEffect, useState } from "react";
+import { RouteComponentProps, useParams } from "react-router";
+
+interface RouteParameters {
+  giverRowKey: string;
+  recipientRowKey: string;
+  partitionKey: string;
+}
+
+interface VerifyConnection extends RouteComponentProps<RouteParameters> {}
+
+const VerifyConnection: React.FC<VerifyConnection> = () => {
+  const { giverRowKey, recipientRowKey, partitionKey } =
+    useParams<RouteParameters>();
+
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    fetch(`verify/${giverRowKey}/${recipientRowKey}/${partitionKey}`, {
+      method: "POST",
+    }).then((response) => setError(!response.ok));
+  }, []);
+
+  if (error)
+    return (
+      <>
+        <h1>
+          En feil oppsto, prøv igjen senere. Hvis feilen vedvarer, ta kontakt
+          med din lokale Gi En Jul administrator.
+        </h1>
+      </>
+    );
+
+  return (
+    <>
+      <h1>Takk til deg!</h1>
+    </>
+  );
+};
+export default VerifyConnection;
