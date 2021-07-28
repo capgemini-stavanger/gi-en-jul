4f06e13f16970195e6f10bb05ad414d414b78eaa
Joachim Andreassen <joachim.andreassen@capgemini.com>
2021-07-28 09:23:53 +0200

    Changed excel download to use apiservice class

--- GiEnJul/ClientApp/src/components/admin/connections/completed/DeliveryExcelDownload.tsx
+++ GiEnJul/ClientApp/src/components/admin/connections/completed/DeliveryExcelDownload.tsx
@@ -1,9 +1,25 @@
 import { Button } from "@material-ui/core";
 import GetAppIcon from "@material-ui/icons/GetApp";
 import React, { useState } from "react";
+import ApiService from "../../../../common/functions/apiServiceClass";
 
 const DeliveryExcelDownload = () => {
   const [location, _] = useState("Nittedal");
+  const apiservice = new ApiService();
+
+  const download = () => {
+    apiservice
+      .get(`/api/admin/excel/delivery/${location}`, { responseType: "blob" })
+      .then((response) => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", `Leveranseliste_${location}.xlsx`);
+        document.body.appendChild(link);
+        link.click();
+      });
+  };
+
   return (
     <Button
       variant="contained"
