268c1bca5f007fa3793c1ae7b936990937681ef6
elisabethnetland <elisabeth.s.netland@gmail.com>
2021-07-28 16:33:44 +0200

    implemented custom breackpoints and responsive design for logo and how component

--- GiEnJul/ClientApp/src/components/landingPage/How.tsx
+++ GiEnJul/ClientApp/src/components/landingPage/How.tsx
@@ -1,65 +1,99 @@
 import * as React from "react";
-import {
-  CssBaseline,
-  Typography,
-  Container,
-  Grid,
-  Box,
-} from "@material-ui/core";
+import { CssBaseline, Typography, Container, Grid, Card } from "@material-ui/core";
 import useStyles from "./Styles";
-import {ExpandMore} from '@material-ui/icons';
+import { ExpandMore } from "@material-ui/icons";
+import dummy from "./../../styling/img/dummy-image.jpg";
 
 const How = () => {
   const classes = useStyles();
-  return(
-  <Container id="how">
-    <CssBaseline />
-    
-      <Typography className={classes.textHeadline} >Hvordan fungerer gi en jul?</Typography>
-      <Grid container className={classes.howContainer}>
-      <Box 
-        className={classes.howItem}
-        boxShadow={1}>
-          <Typography>Alene, eller sammen med vennegjengen, kolleger, familie eller naboer bestemmer du deg for at du vil bli med på Gi en jul</Typography>
-        </Box>
-        <ExpandMore className={classes.nextIcon}/>
-        <Box 
-        className={classes.howItem}
-        boxShadow={1}>
-          <Typography>På gienjul.no registrerer du deg som giver og opplyser antall familiemedlemmer du/dere ønsker å hjelpe</Typography>
-        </Box>
-        <ExpandMore className={classes.nextIcon}/>
-        <Box 
-        className={classes.howItem}
-        boxShadow={1}>
-          <Typography>I mellomtiden jobber barnevernet, NAV og Flyktningtjenesten med å
-          kartlegge og finne familier som trenger hjelp</Typography>
-        </Box>
-        <ExpandMore className={classes.nextIcon}/>
-        <Box 
-        className={classes.howItem}
-        boxShadow={1}>
-          <Typography>Når du har registrert deg får du en mail med info, hvor det står
-          hvordan det fungerer med selve juleeskene, hva som skal oppi, hvor de
-          skal leveres og når</Typography>
-        </Box>
-        <ExpandMore className={classes.nextIcon}/>
-        <Box 
-        className={classes.howItem}
-        boxShadow={1}>
-          <Typography>Så snart jeg får inn familiene matcher jeg familie og giver, og sender
-          en epost til deg med kjønn, alder og gaveønsker til familiemedlemmene.
-          Det er selvsagt helt anonymt.</Typography>
-        </Box>
-        <ExpandMore className={classes.nextIcon}/>
-        <Box 
-        className={classes.howItem}
-        boxShadow={1}>
-          <Typography>Til slutt møtes vi dato i x lokaler, hvor eskene blir kjørt ut av de som jobber i de ulike instansene</Typography>
-        </Box>
-    </Grid>
-  </Container>
-  )
-  };
+  return (
+    <Container id="how" className={classes.howContainer}>
+      <Typography className={classes.textHeadline}>
+        Hvordan fungerer gi en jul?
+      </Typography>
+      <Grid container>
+        <Grid container className={classes.howItem}>
+          <Card>
+            <div className={classes.howText}>
+            <Typography>
+              Alene, eller sammen med vennegjengen, kolleger, familie eller
+              naboer bestemmer du deg for at du vil bli med på Gi en jul
+            </Typography>
+            </div>
+            <img className={classes.howImage} src={dummy}></img>
+          </Card>
+        </Grid>
+        
+        <ExpandMore className={classes.nextIcon} />
+        <Grid container className={classes.howItem}>
+        <Card>
+          <div className={classes.howText}>
+          <Typography>
+            På gienjul.no registrerer du deg som giver og opplyser antall
+            familiemedlemmer du/dere ønsker å hjelpe
+          </Typography>
+          </div>
+          <img className={classes.howImage} src={dummy}></img>
+          </Card>
+        </Grid>
+        <ExpandMore className={classes.nextIcon} />
+
+
+        <Grid container className={classes.howItem}>
+        <Card>
+        <div className={classes.howText}>
+          <Typography>
+            I mellomtiden jobber barnevernet, NAV og Flyktningtjenesten med å
+            kartlegge og finne familier som trenger hjelp
+          </Typography>
+          </div>
+          <img className={classes.howImage} src={dummy}></img>
+          </Card>
+        </Grid>
+
+
+        <ExpandMore className={classes.nextIcon} />
+        <Grid container className={classes.howItem}>
+        <Card>
+        <div className={classes.howText}>
+          <Typography>
+            Når du har registrert deg får du en mail med info, hvor det står
+            hvordan det fungerer med selve juleeskene, hva som skal oppi, hvor
+            de skal leveres og når
+          </Typography>
+          </div>
+          <img className={classes.howImage} src={dummy}></img>
+          </Card>
+        </Grid>
+        <ExpandMore className={classes.nextIcon} />
+        <Grid container className={classes.howItem}>
+        <Card>
+        <div className={classes.howText}>
+          <Typography>
+            Så snart jeg får inn familiene matcher jeg familie og giver, og
+            sender en epost til deg med kjønn, alder og gaveønsker til
+            familiemedlemmene. Det er selvsagt helt anonymt.
+          </Typography>
+          </div>
+          <img className={classes.howImage} src={dummy}></img>
+          </Card>
+        </Grid>
+        <ExpandMore className={classes.nextIcon} />
+        <Grid container className={classes.howItem}>
+        <Card>
+        <div className={classes.howText}>
+          <Typography>
+            Til slutt møtes vi dato i x lokaler, hvor eskene blir kjørt ut av de
+            som jobber i de ulike instansene
+          </Typography>
+          </div>
+          <img className={classes.howImage} src={dummy}></img>
+          </Card>
+        </Grid>
+        <ExpandMore className={classes.dummmyIcon}/>
+      </Grid>
+    </Container>
+  );
+};
 
 export default How;
--- GiEnJul/ClientApp/src/components/landingPage/Styles.tsx
+++ GiEnJul/ClientApp/src/components/landingPage/Styles.tsx
@@ -1,71 +1,116 @@
-import { makeStyles } from '@material-ui/core/styles';
-import { isMobile } from '../../common/functions/IsMobile';
+import { makeStyles } from "@material-ui/core/styles";
+import { isMobile } from "../../common/functions/IsMobile";
 
-const useStyles = makeStyles((theme) => (
-  {
-    root: {
-      backgroundColor: theme.palette.secondary.light,
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.secondary.light,
+  },
+  circle: {
+    borderRadius: "50%",
+    margin: theme.spacing(9, 2, 2, 2),
+    padding: theme.spacing(1, 1),
+    backgroundColor: "white",
+    height: "40%",
+    width: "40%",
+    position: 'static',
+    [theme.breakpoints.up("tablet")]: {
+      height: "30%",
+      width: "30%",
     },
-    circle: {
-      height: '70%',
-      width: '40%', 
-      borderRadius: '50%',
-      margin:theme.spacing(9,2,2,2),
-      padding: theme.spacing(1,1),
-      backgroundColor: 'white',
+    [theme.breakpoints.up("laptop")]: {
+      height: "20%",
+      width: "20%",
     },
-    logoLarge:{
-      height: '50%',
-      width: '60%', 
-      display: 'block',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      margin:theme.spacing(1),
-    }, 
-    logoText: {
-      color: theme.palette.primary.main,
-      fontSize: '250%', 
-      fontWeight: 500,
+  },
+  logoLarge: {
+    height: "44%",
+    width: "52.8%",
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    margin: theme.spacing(1),
+    [theme.breakpoints.up("tablet")]: {
+      height: "35.2%",
+      width: "42.24%",
     },
-    textHeadline:{
-      color: theme.palette.primary.main,
-      fontSize: '200%', 
-      fontWeight: 400,
+    [theme.breakpoints.up("laptop")]: {
+      height: "28.16%",
+      width: "33.8%",
+    },
+  },
+  logoText: {
+    color: theme.palette.primary.main,
+    fontSize: "250%",
+    fontWeight: 500,
+  },
+  textHeadline: {
+    color: theme.palette.primary.main,
+    fontSize: "200%",
+    fontWeight: 400,
+  },
+  howContainer: {
+    marginTop: theme.spacing(2),
+    maxWidth: '85em',
+    maxHeight: '150em',
+  },
+  companyContainer: {
+    marginTop: theme.spacing(2),
+  },
+  howItem: {
+    backgroundColor: "white",
+    margin: theme.spacing(2),
+    borderRadius: "2em",
+    maxWidth: "100%",
+    objectFit: 'cover',
+    display: 'stretch',
+    [theme.breakpoints.up("tablet")]: {
 
+      maxWidth: "33.3%",
     },
-    howContainer: {
-      marginTop: theme.spacing(2),
-    },
-    companyContainer: {
-      marginTop: theme.spacing(2),
-    },
-    howItem: {
-      backgroundColor: 'white',
-      margin: theme.spacing(1),
-      padding: theme.spacing(1), 
-      borderRadius: '2em',
+    [theme.breakpoints.up("laptop")]: {
 
+      maxWidth: "25%",
     },
-    nextIcon: {
-      color: theme.palette.primary.main,
-      margin: 'auto'
-    },
-    questionBox: {
-      margin: theme.spacing(1),
-      borderRadius: '2em',
-      overflow: 'hidden',
-      borderColor: theme.palette.secondary.light,
-      
-    },
-    buttonNext:{
-      color: 'white',
-      backgroundColor: theme.palette.primary.light,
-      margin: theme.spacing(4),
-      textTransform: 'none',
-      "&:hover": {
-        backgroundColor: theme.palette.primary.main,
-      }
-    },
-  }));
+  },
+  howText: {
+    objectFit: 'cover',
+    MaxHeight: '100%',
+    padding: theme.spacing(2),
 
-  export default useStyles;
+  },
+  howImage:{
+    objectFit: 'cover',
+    MaxHeight: '100%',
+    width: '100%', 
+    height: '10em',
+    padding: theme.spacing(0),
+    bottom: theme.spacing(0),
+    display: 'flex',
+    alignItems: 'flex-end',
+  },
+  nextIcon: {
+    color: theme.palette.primary.main,
+    margin: "auto",
+  },
+  dummmyIcon: {
+    color: theme.palette.secondary.light,
+    margin: "auto",
+  },
+  questionBox: {
+    margin: theme.spacing(1),
+    borderRadius: "2em",
+    overflow: "hidden",
+    borderColor: theme.palette.secondary.light,
+  },
+  buttonNext: {
+    color: "white",
+    backgroundColor: theme.palette.primary.light,
+    margin: theme.spacing(4),
+    textTransform: "none",
+    "&:hover": {
+      backgroundColor: theme.palette.primary.main,
+    },
+  },
+}));
+
+export default useStyles;
