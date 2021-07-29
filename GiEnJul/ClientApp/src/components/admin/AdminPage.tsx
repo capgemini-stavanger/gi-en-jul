f7aa4bfaec747ba4825589f1410e5a4f5d22cff6
Odin <odinbjoernebo@gmail.com>
2021-07-29 14:49:09 +0200

    added helper function for readability

--- GiEnJul/ClientApp/src/components/admin/AdminPage.tsx
+++ GiEnJul/ClientApp/src/components/admin/AdminPage.tsx
@@ -24,44 +24,50 @@ function AdminPage() {
     });
   }, []);
 
-  if (location === null) {
-    return (
-      <ErrorPage
-        ErrorText={
-          "Du har ikke blitt tildelt en lokasjon enda, tilkall din GiEnJul admin"
-        }
-      />
-    );
-  } else if (role !== undefined && location !== undefined) {
-    console.log(role);
-    switch (role) {
-      case "Admin":
-        return (
-          <>
-            <NavMenuAdmin />
-            <AdminTab accessToken={userAccessToken} location={location} />
-          </>
-        );
-      case "Institution":
-        return (
-          <>
-            <InstitutionMacro />
-          </>
-        );
+  function getPage() {
+    if (location === null) return "NoLocation";
+    else if (role === undefined || location === undefined) return "Loading";
+    else if (role === "Unspecified") return "Unspecified";
+    else if (role === "Admin") return "Admin";
+    else if (role === "Institution") return "Institution";
+    else return "Error";
+  }
 
-      case "Unspecified":
-        return (
-          <ErrorPage
-            ErrorText={
-              "Du har ikke blitt tildelt en rolle enda, tilkall din GiEnJul admin"
-            }
-          />
-        );
-      default:
-        return <ErrorPage ErrorText={"En Feil har skjedd"} ErrorCode={500} />;
-    }
-  } else {
-    return <LoadingPage />;
+  switch (getPage()) {
+    case "NoLocation":
+      return (
+        <ErrorPage
+          ErrorText={
+            "Du har ikke blitt tildelt en lokasjon enda, tilkall din GiEnJul admin"
+          }
+        />
+      );
+    case "Loading":
+      return <LoadingPage />;
+    case "Unspecified":
+      return (
+        <ErrorPage
+          ErrorText={
+            "Du har ikke blitt tildelt en rolle enda, tilkall din GiEnJul admin"
+          }
+        />
+      );
+    case "Admin":
+      return (
+        <>
+          <NavMenuAdmin />
+          {/*// @ts-ignore */}
+          <AdminTab accessToken={userAccessToken} location={location} />
+        </>
+      );
+    case "Institution":
+      return (
+        <>
+          <InstitutionMacro />
+        </>
+      );
+    default:
+      return <ErrorPage ErrorText={"En Feil har skjedd"} ErrorCode={500} />;
   }
 }
 export default withAuthenticationRequired(AdminPage, {
