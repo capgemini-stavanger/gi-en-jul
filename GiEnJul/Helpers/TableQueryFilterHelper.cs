b15fa19a973a1a25748d5e7c322be62a6e4e92f2
elisabethnetland <elisabeth.s.netland@gmail.com>
2021-07-30 14:59:41 +0200

    naming

--- GiEnJul/Helpers/TableQueryFilterHelper.cs
+++ GiEnJul/Helpers/TableQueryFilterHelper.cs
@@ -24,7 +24,7 @@ namespace GiEnJul.Helpers
 
             return filter;
         }
-        public static string GetAllFilter(string eventName, string location)
+        public static string GetAllActiveEventsFilter(string eventName, string location)
         {
             var PKfilter = TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, $"{eventName}_{location}");
             var eventNameFilter = TableQuery.GenerateFilterCondition("EventName", QueryComparisons.Equal, eventName);
