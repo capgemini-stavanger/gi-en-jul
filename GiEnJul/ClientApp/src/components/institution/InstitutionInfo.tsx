050b3b7ac729eb46fe728f7859d955aecab1289f
Odin Bjørnebo <odinbjoernebo@gmail.com>
2021-08-02 15:22:53 +0200

    added more info and refactored component--

--- GiEnJul/ClientApp/src/components/institution/InstitutionInfo.tsx
+++ GiEnJul/ClientApp/src/components/institution/InstitutionInfo.tsx
@@ -1,42 +1,37 @@
-import { Grid } from "@material-ui/core";
-import { Typography } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import * as React from "react";
 
-const RegistrationInfo = () => (
-  <Grid container spacing={3} direction="column">
-    <Grid item>
-      <Typography variant="h3">Registrer familie</Typography>
+const information: string[] = [
+  "Før du melder inn en familie må familien ha sag ja til å bli med, og du har all informasjon som trengs nedenfor.",
+  "Dersom du har en ID på familien (som PID), må det legges inn.",
+  "Mange givere synes det er fint å kunne kjøpe inn gaver, og ikke bare gavekort.",
+  "Gaveønskene skal ligge på rundt 300 per person. Kom gjerne med flere ønsker, så har giveren noe å velge mellom.",
+  "Fyll inn størrelser dersom de ønsker klær eller sko.",
+  "Alderstilpasset gave betyr at giveren selv kan finne noe som passer til alderen.",
+  "Skriv så detaljerte ønsker som mulig -dersom barnet ikke har ønsker kan du skrive noe om interesser, som fotball, hobbyting, turn osv.",
+  "Husk å skrive dersom familien har spesielle behov.",
+];
+
+const RegistrationInfo = () => {
+  return (
+    <Grid container spacing={3} direction="column">
+      <Grid item>
+        <Typography variant="h3">Registrer familie</Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant="h6">
+          Tusen takk for at du melder inn familie til Gi en jul. Husk:
+        </Typography>
+        <ul>
+          {information.map((point) => (
+            <li>
+              <Typography variant="body1">{point}</Typography>
+            </li>
+          ))}
+        </ul>
+      </Grid>
     </Grid>
-    <Grid item>
-      <Typography variant="h6">
-        Tusen takk for at du melder inn familie til Gi en jul. Husk:
-      </Typography>
-      <ul>
-        <li>
-          <Typography variant="body1">
-            Fyll inn størrelser dersom de ønsker klær eller sko.
-          </Typography>
-        </li>
-        <li>
-          <Typography variant="body1">
-            Alderstilpasset gave betyr at giveren selv kan finne noe som passer
-            til alderen.
-          </Typography>
-        </li>
-        <li>
-          <Typography variant="body1">
-            Skriv så detaljerte ønsker som mulig -dersom barnet ikke har ønsker
-            kan du skrive noe om interesser, som fotball, hobbyting, turn osv.
-          </Typography>
-        </li>
-        <li>
-          <Typography variant="body1">
-            Husk å skrive dersom familien har spesielle behov.
-          </Typography>
-        </li>
-      </ul>
-    </Grid>
-  </Grid>
-);
+  );
+};
 
 export default RegistrationInfo;
