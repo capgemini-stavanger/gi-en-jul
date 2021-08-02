75f7a418da4bcf16a89945686aa47fd49939b8db
elisabethnetland <elisabeth.s.netland@gmail.com>
2021-08-02 10:12:49 +0200

    implemented isMobile on stepper for user experience

--- GiEnJul/ClientApp/src/components/registerGiver/Pager.tsx
+++ GiEnJul/ClientApp/src/components/registerGiver/Pager.tsx
@@ -1,7 +1,8 @@
-import { Button, MobileStepper } from "@material-ui/core";
+import { Button, Grid, MobileStepper } from "@material-ui/core";
 import React, { FC } from "react";
 import useStyles from "./Styles";
 import { ArrowBackIos, ArrowForwardIos } from "@material-ui/icons";
+import { isMobile } from "../../common/functions/IsMobile";
 interface IPager {
   onBack?: (event: React.FormEvent) => void;
   onContinue?: (event: React.FormEvent) => void;
@@ -18,56 +19,89 @@ const Pager: FC<IPager> = ({
   step,
 }) => {
   const classes = useStyles();
-  return (
-    <>
-      {step == undefined ? (
-        <>
-          <Button
-            className={classes.buttonNext}
-            endIcon={<ArrowForwardIos />}
-            onClick={onContinue}
-          >
-            {continueText}
-          </Button>
-        </>
-      ) : (
-        <MobileStepper
-          variant="dots"
-          steps={4}
-          position="static"
-          activeStep={step-1}
-          className={classes.buttons}
-          nextButton={
-            onContinue && (
-              <Button
-                className={classes.buttonNext}
-                endIcon={<ArrowForwardIos />}
-                onClick={onContinue}
-              >
-                {continueText}
-              </Button>
-            )
-          }
-          backButton={
-            onBack && (
-              <Button
-                className={classes.buttonBack}
-                startIcon={<ArrowBackIos />}
-                onClick={onBack}
-              >
-                {backText}
-              </Button>
-            )
-          }
-        />
-      )}
-    </>
-  );
+  if (isMobile()) {
+    return (
+      <>
+        {step == undefined ? (
+          <>
+            <Button
+              className={classes.buttonNext}
+              endIcon={<ArrowForwardIos />}
+              onClick={onContinue}
+            >
+              {continueText}
+            </Button>
+          </>
+        ) : (
+          <MobileStepper
+            variant="dots"
+            steps={4}
+            position="bottom"
+            activeStep={step - 1}
+            className={classes.buttons}
+            nextButton={
+              onContinue && (
+                <Button
+                  className={classes.buttonNext}
+                  endIcon={<ArrowForwardIos />}
+                  onClick={onContinue}
+                >
+                  {continueText}
+                </Button>
+              )
+            }
+            backButton={
+              onBack && (
+                <Button
+                  className={classes.buttonBack}
+                  startIcon={<ArrowBackIos />}
+                  onClick={onBack}
+                >
+                  {backText}
+                </Button>
+              )
+            }
+          />
+        )}
+      </>
+    );
+  } else {
+    return (
+      <Grid
+        container
+        spacing={2}
+        justifyContent="center"
+        className={classes.buttons}
+      >
+        {onBack && (
+          <Grid item>
+            <Button
+              className={classes.buttonBack}
+              startIcon={<ArrowBackIos />}
+              onClick={onBack}
+            >
+              {backText}
+            </Button>
+          </Grid>
+        )}
+        {onContinue && (
+          <Grid item>
+            <Button
+              className={classes.buttonNext}
+              endIcon={<ArrowForwardIos />}
+              onClick={onContinue}
+            >
+              {continueText}
+            </Button>
+          </Grid>
+        )}
+      </Grid>
+    );
+  }
 };
 
 Pager.defaultProps = {
   backText: "Tilbake",
   continueText: "Neste",
 };
-
 export default Pager;
