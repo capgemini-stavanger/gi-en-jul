954ccf88dc0f7ddff9973ad625f344f91d97c89f
elisabethnetland <elisabeth.s.netland@gmail.com>
2021-08-03 08:53:34 +0200

    add padding to institutionPage

---
+++ GiEnJul/ClientApp/src/components/institution/Styles.tsx
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    root:{
+        padding: theme.spacing(7),
+    },
+
+}));
+
+export default useStyles;
