c64e9d1a48f0b9f2d07a3feae883379047447106
Joachim Andreassen <61537815+joffe97@users.noreply.github.com>
2021-08-05 10:04:33 +0200

    Update GiEnJul/ClientApp/src/components/registerGiver/PrivacyDialog.tsx
    Co-authored-by: Odin Bjørnebo <46569868+TheseusDeus@users.noreply.github.com>

--- GiEnJul/ClientApp/src/components/registerGiver/PrivacyDialog.tsx
+++ GiEnJul/ClientApp/src/components/registerGiver/PrivacyDialog.tsx
@@ -35,7 +35,7 @@ const PrivacyTexts = {
     "Vi lagrer følgende personopplysninger om våre kunder:navn, adresse, telefonnummer, e-postadresse.",
     "Vi behandler opplysningene for å kunne gjennomføre prosjektet. Personopplysninger vil bli brukt for å koble givere og familier, informasjonsflyt angående prosjektet og generell kontakt om det er nødvendig.",
     "Informasjon om navn, telefon, e-postadresse benyttes for å oppfylle avtalen. Grunnlaget for denne behandlingen er personvernforordningens artikkel Art 6 (b).",
-    "Vi lagrer de personopplysningene du har avgitt på våre nettsider.Vi bruker informasjonskapsler/cookies på våre nettsider for å gi deg som besøker siden best kundeopplevelse og service.Lov om elektronisk kommunikasjon krever at vi informerer våre besøkende om bruk av informasjonskapsler (cookies). Les mer om bruk av informasjonskapsler her:",
+    "Vi lagrer de personopplysningene du har avgitt på våre nettsider.",
     "Vi vil ikke dele, selge, overføre eller på annen måte utlevere personopplysninger til andre, med mindre vi er rettslig forpliktet til det.",
    "Opplysninger vi har mottatt i forbindelse med din deltagelse i Gi en jul lagres i vårt aktive database i 13 måneder.",
     "Vi behandler dine personopplysninger i henhold til personopplysningsloven og gjeldende forskrifter. Det gjøres oppmerksom på at du kan kreve innsyn i og flytting av egne personopplysninger, samt kreve retting eller sletting av opplysninger. Det kan klages til Datatilsynet på behandling i strid med reglene.",
