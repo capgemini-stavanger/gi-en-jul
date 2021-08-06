9c7397c8f2a2e86b61ac4222ed435e0815ecc5f3
Sommerli <45152025+Sommerli@users.noreply.github.com>
2021-08-06 09:58:17 +0200

    Email messages changes (#386)
    * small changes
    
    * Email message changes

--- GiEnJul/Models/Person.cs
+++ GiEnJul/Models/Person.cs
@@ -8,6 +8,25 @@
         public string Wish { get; set; }
         public int Age { get; set; }
         public Gender Gender { get; set; }
+
+        public string ToReadableString()
+        {
+            return $"{GenderToString()} {Age} år, {(string.IsNullOrEmpty(Wish) ? " Ønske ikke spesifisert" : Wish)}";
+        }
+
+        private string GenderToString()
+        {
+            switch (Gender)
+            {
+                case Gender.Female:
+                    return Age > 18 ? "Kvinne" : "Jente";
+                case Gender.Male:
+                    return Age > 18 ? "Mann" : "Gutt";
+                case Gender.Unspecified:
+                default:
+                    return "Ikke spesifisert";
+            }
+        }
     }
 
     public enum Gender
