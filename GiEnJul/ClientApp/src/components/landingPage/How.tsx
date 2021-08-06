36fe4c2c140b63bb9c59591c24505896fa938c09
elisabethnetland <37541100+elisabethnetland@users.noreply.github.com>
2021-08-06 08:27:05 +0200

    Landing page styling (#387)
    * tekst
    
    * text landingpage
    
    * test
    
    * newHow
    
    * fix how section

--- GiEnJul/ClientApp/src/components/landingPage/How.tsx
+++ GiEnJul/ClientApp/src/components/landingPage/How.tsx
@@ -2,15 +2,34 @@ import * as React from "react";
 import { Typography, Container, Grid, Card } from "@material-ui/core";
 import useStyles from "./Styles";
 import { ExpandMore } from "@material-ui/icons";
-import dummy from "./../../styling/img/dummy-image.jpg";
-import { isMobile } from "../../common/functions/IsMobile";
+import step1 from "./../../styling/img/step1.svg";
+import step2 from "./../../styling/img/step2.svg";
+import step3 from "./../../styling/img/step3.svg";
 
 const How = () => {
   const classes = useStyles();
   return (
     <Container id="how" className={classes.sectionContainer}>
+      <Grid container justifyContent="center" alignItems="flex-start">
+        <div className={classes.headLineContainer}>
+          <Typography className={classes.textHeadline}>
+            Hva er <br /> Gi en jul?
+          </Typography>
+          <ExpandMore color="primary" />
+          <div className={classes.textContainer}>
+            <Typography className={classes.paragraph}>
+              Gi en jul er et veldedighets-prosjekt i samarbeid med blant andre
+              barnevernet og NAV. <br />
+              Som giver til “Gi en jul” donerer du julemiddag og julegaver til
+              en familie som virkelig trenger det. Dette er foreldre og barn som
+              ikke selv har mulighet til å kjøpe en skikkelig julemiddag,
+              julesnop eller julegaver til hverandre.
+            </Typography>
+          </div>
+        </div>
+      </Grid>
       <div className={classes.headLineContainer}>
-        <Typography className={classes.textHeadline}>
+        <Typography className={classes.blueTextHeadline}>
           Hvordan fungerer <br /> Gi en jul?
         </Typography>
         {/* <ExpandMore />
@@ -25,79 +44,56 @@ const How = () => {
           julemiddag, julesnop eller julegaver til hverandre.
         </Typography> */}
       </div>
-      <Grid container justifyContent="center">
-        <Grid container className={classes.howItem}>
-          <Card>
-            <div className={classes.howText}>
-              <Typography>
-                Alene, eller sammen med vennegjengen, kolleger, familie eller
-                naboer bestemmer du deg for at du vil bli med på Gi en jul.
+      <Grid container justifyContent="center" alignItems="flex-start">
+        <Grid item className={classes.howItem}>
+          <div className={classes.imgContainer}>
+            <img className={classes.stepsImage} src={step1}></img>
+          </div>
+          <div className={classes.headLineContainer}>
+            <Typography className={classes.blueTextHeadline}>
+              Steg 1{" "}
+            </Typography>
+            <div className={classes.textContainer}>
+              <Typography className={classes.paragraph}>
+                Registrer deg som giver alene, med venner, familie eller
+                kolleger via “Bli giver” knappen øverst på denne siden.
               </Typography>
             </div>
-            <img className={classes.howImage} src={dummy}></img>
-          </Card>
+          </div>
         </Grid>
-        {isMobile() ? <ExpandMore className={classes.nextIcon} /> : <></>}
-        <Grid container className={classes.howItem}>
-          <Card>
-            <div className={classes.howText}>
-              <Typography>
-                På gienjul.no registrerer du deg som giver og opplyser antall
-                familiemedlemmer du/dere ønsker å hjelpe.
+        <Grid item className={classes.howItem}>
+          <div className={classes.imgContainer}>
+            <img className={classes.stepsImage} src={step2}></img>
+          </div>
+          <div className={classes.headLineContainer}>
+            <Typography className={classes.blueTextHeadline}>
+              Steg 2{" "}
+            </Typography>
+            <div className={classes.textContainer}>
+              <Typography className={classes.paragraph}>
+                Når julen nærmer seg, får du tildelt en anonym familie som du
+                handler til. Du mottar en liste med gaveønsker og hva de ønsker
+                å spise.
               </Typography>
             </div>
-            <img className={classes.howImage} src={dummy}></img>
-          </Card>
+          </div>
         </Grid>
-        {isMobile() ? <ExpandMore className={classes.nextIcon} /> : <></>}
-        <Grid container className={classes.howItem}>
-          <Card>
-            <div className={classes.howText}>
-              <Typography>
-                I mellomtiden jobber barnevernet, NAV og Flyktningtjenesten med
-                å kartlegge og finne familier som trenger hjelp.
+
+        <Grid item className={classes.howItem}>
+          <div className={classes.imgContainer}>
+            <img className={classes.stepsImage} src={step3}></img>
+          </div>
+          <div className={classes.headLineContainer}>
+            <Typography className={classes.blueTextHeadline}>
+              Steg 3{" "}
+            </Typography>
+            <div className={classes.textContainer}>
+              <Typography className={classes.paragraph}>
+                På innsamlingsdagen leverer du esken et angitt sted, og
+                barnevernet og andre kjører eskene ut til familiene.
               </Typography>
             </div>
-            <img className={classes.howImage} src={dummy}></img>
-          </Card>
-        </Grid>
-        {isMobile() ? <ExpandMore className={classes.nextIcon} /> : <></>}
-        <Grid container className={classes.howItem}>
-          <Card>
-            <div className={classes.howText}>
-              <Typography>
-                Når du har registrert deg får du en mail med info, hvor det står
-                hvordan det fungerer med selve juleeskene, hva som skal oppi,
-                hvor de skal leveres og når.
-              </Typography>
-            </div>
-            <img className={classes.howImage} src={dummy}></img>
-          </Card>
-        </Grid>
-        {isMobile() ? <ExpandMore className={classes.nextIcon} /> : <></>}
-        <Grid container className={classes.howItem}>
-          <Card>
-            <div className={classes.howText}>
-              <Typography>
-                Så snart jeg får inn familiene matcher jeg familie og giver, og
-                sender en epost til deg med kjønn, alder og gaveønsker til
-                familiemedlemmene. Det er selvsagt helt anonymt.
-              </Typography>
-            </div>
-            <img className={classes.howImage} src={dummy}></img>
-          </Card>
-        </Grid>
-        {isMobile() ? <ExpandMore className={classes.nextIcon} /> : <></>}
-        <Grid container className={classes.howItem}>
-          <Card>
-            <div className={classes.howText}>
-              <Typography>
-                Til slutt møtes vi x dato i x lokaler, hvor eskene blir kjørt ut
-                av de som jobber i de ulike instansene.
-              </Typography>
-            </div>
-            <img className={classes.howImage} src={dummy}></img>
-          </Card>
+          </div>
         </Grid>
       </Grid>
     </Container>
