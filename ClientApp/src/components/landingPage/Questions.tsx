aed06806a26c50a0142f6c83bdae89f517f31c89
Tor-Fredrik Torgersen <tor-fredrik.torgersen@capgemini.com>
2021-09-22 15:27:49 +0200

    updated FAQs to be rendered dynamically and fetch question from separate file

--- ClientApp/src/components/landingPage/Questions.tsx
+++ ClientApp/src/components/landingPage/Questions.tsx
@@ -9,7 +9,7 @@ import {
   Grid,
 } from "@material-ui/core";
 import useStyles from "./Styles";
-
+import {faq} from "../../common/constants/FAQsource"
 
 const Questions = () => {
   const classes = useStyles();
@@ -27,103 +27,26 @@ const Questions = () => {
         </Typography>
       </div>
       <Grid container justifyContent="center">
-        <Grid className={classes.questionItem}>
-          <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')} className={classes.questionBox}>
-            <AccordionSummary
+        {Array.from(faq).map((val,index)=>(
+          
+          <Grid className={classes.questionItem} key={index}>
+          <Accordion  expanded={expanded === index.toString()} onChange={handleChange(index.toString())}>
+          <AccordionSummary
               className={classes.questionSummary}
               expandIcon={
                 <ExpandMoreIcon className={classes.questionSummary} />
               }
             >
-              <Typography>Hva koster det å gi en jul?</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Juleeskene skal minimum inneholde ubrukte gaver til minimum 300
-                per person, og en julemiddag og dessert til hele familien. Hvis
-                ønskelig kan dere bidra med en ekstra middag, som pølse og
-                potetmos, medisterkaker og poteter, julepølse og poteter.
-              </Typography>
-            </AccordionDetails>
+              <Typography>{val.question}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  {val.answer}
+                </Typography>
+              </AccordionDetails>
           </Accordion>
-        </Grid>
-
-        <Grid className={classes.questionItem}>
-          <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')} className={classes.questionBox}>
-            <AccordionSummary
-              className={classes.questionSummary}
-              expandIcon={
-                <ExpandMoreIcon className={classes.questionSummary} />
-              }
-            >
-              <Typography>Når får jeg tildelt en familie?</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Familiene deles ut fortløpende, og vi prøver å gi deg familien
-                din i god tid før jul.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        </Grid>
-
-        <Grid className={classes.questionItem}>
-          <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}  className={classes.questionBox}>
-            <AccordionSummary
-              className={classes.questionSummary}
-              expandIcon={
-                <ExpandMoreIcon className={classes.questionSummary} />
-              }
-            >
-              <Typography>Kan jeg legge oppi noe ekstra?</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Dersom du har noe pent brukt som passer til alderen, kan du
-                legge det oppi. Merk: Dette erstatter ikke julegaven, og det må
-                være i god stand.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        </Grid>
-
-        <Grid className={classes.questionItem}>
-          <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}  className={classes.questionBox}>
-            <AccordionSummary
-              className={classes.questionSummary}
-              expandIcon={
-                <ExpandMoreIcon className={classes.questionSummary} />
-              }
-            >
-              <Typography>Kan jeg velge familie selv?</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Du kan komme med ønske for antall familiemedlemmer.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        </Grid>
-
-        <Grid className={classes.questionItem}>
-          <Accordion expanded={expanded === 'panel5'} onChange={handleChange('panel5')}  className={classes.questionBox}>
-            <AccordionSummary
-              className={classes.questionSummary}
-              expandIcon={
-                <ExpandMoreIcon className={classes.questionSummary} />
-              }
-            >
-              <Typography>Hvordan vet jeg hva familien ønsker seg?</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Når du får tildelt familie vil du også få vite hva de ønsker
-                seg. Dersom de ikke har kommet med ønsker, er lue, skjerf,
-                votter, lys, servietter, sjokolade og pledd noen tips.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        </Grid>
+          </Grid>
+        ))}
       </Grid>
     </Container>
   );
