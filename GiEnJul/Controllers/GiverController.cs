81b2abcb5d3fbd27fce0f083856936f9a7ab3a89
Tor-Fredrik Torgersen <tor-fredrik.torgersen@capgemini.com>
2021-09-30 14:56:34 +0200

    added dynamic mail1

--- GiEnJul/Controllers/GiverController.cs
+++ GiEnJul/Controllers/GiverController.cs
@@ -7,6 +7,7 @@ using GiEnJul.Utilities;
 using Microsoft.AspNetCore.Mvc;
 using Serilog;
 using System.Linq;
+using System;
 using System.Threading.Tasks;
 
 namespace GiEnJul.Controllers
@@ -55,20 +56,63 @@ namespace GiEnJul.Controllers
 
             var insertedAsDto = _mapper.Map<PostGiverResultDto>(await _giverRepository.InsertOrReplaceAsync(giver));
 
-            var givers_in_event = await _giverRepository.GetGiversByLocationAsync(active_event, giverDto.Location);
-            var num_givers = givers_in_event.Count();
-            bool waiting_list = num_givers > giver_limit;
+            //var eventName = await _eventRepository.GetActiveEventForLocationAsync(giver.Location);
+            var contacts = await _eventRepository.GetContactsWithActiveEventAsync();
+
+            Console.WriteLine(contacts);
+            var eventDto = contacts.First(x => x.RowKey == giverDto.Location); 
+            //foreach(Event eventet in contacts)
+            //{
+             //   Console.Write(eventet.RowKey);
+               // if (eventet.RowKey == giverDto.Location)
+             //   {
+             //       eventDto = eventet;
+            //    }
+            //}
+            //var eventDto = _mapper.Map<PostEventDto>(await _eventRepository.InsertOrReplaceAsync(city));
+
+
+
+            //var givers_in_event = await _giverRepository.GetGiversByLocationAsync(active_event, giverDto.Location);
+            //var num_givers = givers_in_event.Count();
+            //bool waiting_list = num_givers > giver_limit;
+            //$"{(waiting_list ? $"</br> Tusen takk for at du har meldt deg som giver til årets Gi en jul. Grunnet stor pågang har du havnet i ventelisten.</br>Din posisjon i ventelisten er: {num_givers - giver_limit}.</br></br>" : "Du slapp ventelisten.")}" +
+
 
             var messageContent =
-                $"<h2>Hjertelig takk, {insertedAsDto.FullName}!</h2>" +
-                $"Da er du registrert! Vit at familiene setter utrolig stor pris på dette, " +
-                $"og barnevernet forteller om mange tårevåte øyeblikk når familiene får eskene sine.<br/>" +
-                $"{(waiting_list ? $"</br>Grunnet stor pågang har du havnet i ventelisten.</br>Din posisjon i ventelisten er: {num_givers-giver_limit}.</br></br>" : "")}" + 
-                $"Når det nærmer seg jul vil du motta mer informasjon om familien du skal gi en jul. " +
-                $"Da får du også ønskelister, sted og tidspunkt for innlevering av juleesken.</br></br>" +
-                $"Følg også med på Gi en jul sin <a href='https://www.facebook.com/gienjul'>Facebook - side</a>. Igjen tusen takk, ditt bidrag betyr veldig mye!";
+                $"Hei! <br/>" +
 
-            await _emailClient.SendEmailAsync(insertedAsDto.Email, insertedAsDto.FullName, "Takk for din registrering!", messageContent);
+
+                $"Tusen takk for at du har meldt deg som giver til årets Gi en jul. Så snart vi har en familie til deg," +
+                $"vil du motta en epost med mer informasjon. Vi deler ut familier fortløpende, og inntil et par uker før innlevering. <br/><br/>" +
+
+                $"Juleeskene skal i år leveres {eventDto.DeliveryTime}, {eventDto.DeliveryDate}, <a href={eventDto.DeliveryGPS}>{eventDto.DeliveryAddress}</a>.<br/><br/>" +
+
+                $"Juleeskene skal minst inneholde en julemiddag med dessert og en gave til hvert av familiemedlemmene. Du får vite mat-og gaveønsker når du får familien. <br/><br/>" +
+
+                $"Dersom du ønsker, kan du bidra med én ekstra middag og/eller noe til julefrokosten. Middagen kan <br/>" +
+                $"eksempelvis være pølse og potetmos, medisterkaker og poteter, kjøttdeig og spaghetti, eller noe annet du synes er passende. <br/><br/>" +
+
+                $"Har du lyst til å legge mer oppi esken, er det selvsagt frivillig. Forslag til ekstra-ting, er: <br/>" +
+                $"<ul> <li> julestrømpe med godteri til barna </li><li> saft, juice, melk, te, kaffe </li>" +
+                $"<li> frukt </li><li> snacks og julegodteri</li><li> julekaker</li><li> pålegg: Nugatti, leverpostei, kjøttpålegg, ost og så videre..</li>" +
+                $"<li> servietter, lys og julepynt</li><li> brød, julekake</li></ul><br/>" +
+                $"Pass på at ikke maten blir dårlig/sur, og vær obs på datostempel. Ikke kjøp alkoholholdig drikke! <br/>" +
+                $"<h3>Innkjøp av julegaver </h3>" +
+                $"Husk at det er gaveønsker og ikke handleliste som må følges til punkt og prikke. Dersom ønskene er gavekort, kan du likevel kjøpe en gave du synes passer til alderen.<br/><br/>" +
+
+                $"Gaver pakkes inn og merkes med til mor, til far, til jente x år, til gutt x år og så videre." +
+                $"Det er lurt å legge byttelapp oppi esken. Pakk gjerne i bananesker, eller andre esker som er enkle å bære.<br/><br/>" +
+
+                $"NB! Dersom du ønsker å gi bort brukte leker eller tøy, er det viktig at dette er i god stand, og ikke erstatter julegaven." +
+                $"Vi støtter selvsagt gjenbruk, men dette er familier som sjeldent kan unne seg nye ting.<br/><br/>" +
+
+                $"Igjen vil vi si tusen takk for at du har meldt seg som giver! Vi håper du har fått informasjonen du trenger, <br/>" +
+                $"og lurer du på noe i mellomtiden ber vi deg ta en titt på ofte stile spørsmål på <a href='https://gienjul.no'>nettsiden<a/>, og følg gjerne med på <a href='https://www.facebook.com/gienjul'>facebook-eventet<a/>.<br/><br/>" +
+                $"Vennlig hilsen {eventDto.ContactPerson}";
+
+
+            await _emailClient.SendEmailAsync(insertedAsDto.Email, insertedAsDto.FullName, "Gi en jul - registrering og informasjon!", messageContent);
 
             return CreatedAtAction(nameof(insertedAsDto), insertedAsDto);
         }
