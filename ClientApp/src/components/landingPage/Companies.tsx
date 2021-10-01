bfaa177493c8f9ada0650f39827360d1befeb0be
Sommerli <martinsommerli@gmail.com>
2021-10-01 12:44:21 +0200

    New companies text

--- ClientApp/src/components/landingPage/Companies.tsx
+++ ClientApp/src/components/landingPage/Companies.tsx
@@ -11,7 +11,7 @@ const Companies = () => {
         <Typography className={classes.textHeadline}>For bedrifter</Typography>
       </div>
       <Typography className={classes.contactContent}>
-      Hvert år er det mange lokale bedrifter som bidrar til Gi en jul, og det er flere måter å gjøre det på. Ta kontakt med Gi en jul i din kommune. Kontaktinformasjon finner du under her.
+      Dersom du ønsker å få med kolleger og gi en jul, registrerer du dere som givere som vanlig. Andre måter å bidra på er å gi et pengebeløp som kan benyttes til gavekort på en opplevelse for barn, eller noe annet vi kan putte i eskene, som konfekt, godteri og lignende. Ta kontakt med kontaktpersonen i din kommune, så kan vi snakkes mer om hvordan din bedrift kan bidra.
       </Typography>
     </Container>
   );
