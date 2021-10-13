23dca10623940e701962c318d0220099bb0dbaf0
Tor-Fredrik Torgersen <tor-fredrik.torgersen@capgemini.com>
2021-10-13 13:24:02 +0200

    edited facebook link

--- GiEnJul/Controllers/GiverController.cs
+++ GiEnJul/Controllers/GiverController.cs
@@ -107,7 +107,7 @@ namespace GiEnJul.Controllers
                 $"Vi støtter selvsagt gjenbruk, men dette er familier som sjeldent kan unne seg nye ting.<br/><br/>" +
 
                 $"Igjen vil vi si tusen takk for at du har meldt seg som giver! Vi håper du har fått informasjonen du trenger, <br/>" +
-                $"og lurer du på noe i mellomtiden ber vi deg ta en titt på ofte stilte spørsmål på <a href='https://gienjul.no'>nettsiden<a/>, og følg gjerne med på <a href='https://www.facebook.com/gienjul'>facebook-eventet<a/>.<br/><br/>" +
+                $"og lurer du på noe i mellomtiden ber vi deg ta en titt på ofte stilte spørsmål på <a href='https://gienjul.no'>nettsiden<a/>, og følg gjerne med på <a href={eventDto.Facebook}'>Facebook-eventet<a/>.<br/><br/>" +
                 $"Vennlig hilsen {eventDto.ContactPerson}";
 
             try
