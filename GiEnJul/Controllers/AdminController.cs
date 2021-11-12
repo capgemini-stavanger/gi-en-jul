e6d3fb0bd5fc156d23c69be535a7777582d18c7a
Sommerli <martinsommerli@gmail.com>
2021-11-12 11:18:09 +0100

    Added filter to overview-listings

--- GiEnJul/Controllers/AdminController.cs
+++ GiEnJul/Controllers/AdminController.cs
@@ -61,7 +61,7 @@ namespace GiEnJul.Controllers
         {
             var activeEvent = await _eventRepository.GetActiveEventForLocationAsync(location);
             var givers = await _giverRepository.GetGiversByLocationAsync(activeEvent, location);
-            return givers.OrderBy(x => x.RegistrationDate).ToList();
+            return givers.OrderBy(x => x.HasConfirmedMatch).ThenBy(x => x.IsSuggestedMatch).ThenBy(x => x.RegistrationDate).ToList();
         }
 
         [HttpGet("Overview/Recipients")]
@@ -74,7 +74,7 @@ namespace GiEnJul.Controllers
             {
                 recipient.FamilyMembers = await _personRepository.GetAllByRecipientId(recipient.RowKey);
             }
-            return recipients;
+            return recipients.OrderBy(x => x.HasConfirmedMatch).ThenBy(x => x.IsSuggestedMatch).ToList();
         }
 
         [HttpGet("excel/delivery/{location}")]
