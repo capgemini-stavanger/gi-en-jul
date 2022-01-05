d1fd2f9a9f6886822c7e2ae504ef05f5bb412e5f
Håvard <haavard.godal@gmail.com>
2022-01-05 10:58:31 +0100

    Formating some files

--- ClientApp/src/components/shared/navbar/NavBarPublic.tsx
+++ ClientApp/src/components/shared/navbar/NavBarPublic.tsx
@@ -33,7 +33,7 @@ const NavBarPublic = () => {
       <>
         <AppBar className={classes.navContainer}>
           <Toolbar className={classes.toolBar}>
-            <IconButton >
+            <IconButton>
               <Link to="/">
                 <img className={classes.smallLogo} src={logo}></img>
               </Link>
@@ -57,40 +57,52 @@ const NavBarPublic = () => {
             <Drawer open={!!anchorEl} anchor="top" onClose={handleClose}>
               <List className={classes.drawerMenu}>
                 <IconButton
-                onClick={handleClose}
-                className={classes.closeButton}>
-                <Close color="primary"/>
+                  onClick={handleClose}
+                  className={classes.closeButton}
+                >
+                  <Close color="primary" />
                 </IconButton>
-                <ListItem >
+                <ListItem>
                   <Scroll onClick={handleClose} to="how" smooth={true}>
-                    <Typography className={classes.drawerContent}>Hvordan fungerer gi en jul?</Typography>
+                    <Typography className={classes.drawerContent}>
+                      Hvordan fungerer gi en jul?
+                    </Typography>
                   </Scroll>
                 </ListItem>
                 <ListItem>
                   <Scroll onClick={handleClose} to="questions" smooth={true}>
-                  <Typography className={classes.drawerContent}>Ofte stilte spørsmål</Typography>
-                  </Scroll>
-                </ListItem >
-                <ListItem>
-                  <Scroll onClick={handleClose} to="companies" smooth={true}>
-                  <Typography className={classes.drawerContent}>For bedrifter</Typography>
+                    <Typography className={classes.drawerContent}>
+                      Ofte stilte spørsmål
+                    </Typography>
                   </Scroll>
                 </ListItem>
-                <ListItem >
+                <ListItem>
+                  <Scroll onClick={handleClose} to="companies" smooth={true}>
+                    <Typography className={classes.drawerContent}>
+                      For bedrifter
+                    </Typography>
+                  </Scroll>
+                </ListItem>
+                <ListItem>
                   <Scroll onClick={handleClose} to="contact" smooth={true}>
-                  <Typography className={classes.drawerContent}>Kontakt</Typography>
+                    <Typography className={classes.drawerContent}>
+                      Kontakt
+                    </Typography>
                   </Scroll>
                 </ListItem>
               </List>
             </Drawer>
-              <Button
-                size="large"
-                endIcon={<ArrowForwardIos />}
-                className={classes.buttonNext}
-                onClick={React.useCallback(() => history.push("/bli-giver"), [history])}
-              >
-                Bli giver
-              </Button>
+            <Button
+              size="large"
+              endIcon={<ArrowForwardIos />}
+              className={classes.buttonNext}
+              onClick={React.useCallback(
+                () => history.push("/bli-giver"),
+                [history]
+              )}
+            >
+              Bli giver
+            </Button>
           </Toolbar>
         </AppBar>
       </>
