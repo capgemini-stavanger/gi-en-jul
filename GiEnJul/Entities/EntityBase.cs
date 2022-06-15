c3a88ee08be6079e7e7d53ab0837a368b1f90240
RonHaland <ronny.haland@gmail.com>
2022-06-15 08:31:14 +0200

    Remove code no longer needed

--- GiEnJul/Entities/EntityBase.cs
+++ GiEnJul/Entities/EntityBase.cs
@@ -1,8 +1,6 @@
 ﻿using Azure;
 using Azure.Data.Tables;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace GiEnJul.Entities
 {
@@ -21,35 +19,5 @@ namespace GiEnJul.Entities
         public string RowKey { get; set; }
         public DateTimeOffset? Timestamp { get; set; }
         public ETag ETag { get; set; }
-
-        //public void ReadEntity(IDictionary<string, EntityProperty> properties, OperationContext operationContext)
-        //{
-        //    var errorList = new List<string>();
-
-        //    foreach (var entityProperty in GetType().GetProperties())
-        //    {
-        //        if (properties.TryGetValue(entityProperty.Name, out var value))
-        //        {
-        //            try
-        //            {
-        //                entityProperty.SetValue(this, value.PropertyAsObject);
-        //            }
-        //            catch (ArgumentException ex)
-        //            {
-        //                errorList.Add($"{entityProperty.Name} : {ex.Message}");
-        //            }
-        //        }
-        //    }
-
-        //    if (errorList.Any())
-        //    {
-        //        throw new ArgumentException($"{GetType().Name}{Environment.NewLine}{string.Join(Environment.NewLine, errorList)}");
-        //    }
-        //}
-
-        //public IDictionary<string, EntityProperty> WriteEntity(OperationContext operationContext)
-        //{
-        //    return TableEntity.Flatten(this, operationContext);
-        //}
     }
 }
