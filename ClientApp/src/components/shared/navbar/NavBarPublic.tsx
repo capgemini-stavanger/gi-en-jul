24aa5707d720632aeeaf498bcfcea7196c249892
hildmi <hildem90@gmail.com>
2022-06-29 13:45:23 +0200

    added onClick and styling in navbar

--- ClientApp/src/components/shared/navbar/NavBarPublic.tsx
+++ ClientApp/src/components/shared/navbar/NavBarPublic.tsx
@@ -108,7 +108,13 @@ const NavBarPublic = () => {
                   </Link>
                 </ListItem>
                 <ListItem>
-                  <Link to="kommune">
+                  <Link
+                    to="kommune"
+                    onClick={() => {
+                      history.push("/kommune");
+                    }}
+                    style={{ textDecoration: "none" }}
+                  >
                     <Typography className={classes.drawerContent}>Kommuner</Typography>
                   </Link>
                 </ListItem>
