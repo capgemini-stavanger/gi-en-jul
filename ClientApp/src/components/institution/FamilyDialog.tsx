1f22a0adfb45af699926e605b2a0afd63a695eda
Magnus Vedahl Gulbrandsen <magnus.vedahl-gulbrandsen@capgemini.com>
2022-06-29 09:21:01 +0200

    EditFamily remake, proper behavior, use of Grid

--- ClientApp/src/components/institution/FamilyDialog.tsx
+++ ClientApp/src/components/institution/FamilyDialog.tsx
@@ -1,14 +1,20 @@
  import {
+     Box,
      Button,
      Dialog,
      DialogActions,
      DialogContent,
      DialogTitle,
+     IconButton,
+     Input,
+     TextField,
  } from "@material-ui/core";
  import {RecipientType} from "components/shared/Types";
  import ApiService from "common/functions/apiServiceClass";
  import { useEffect, useState} from "react";
  import { DataGrid, GridColDef } from '@material-ui/data-grid';
+import EditFamilyDialog from "components/shared/EditFamilyDialog";
+import EditIcon from '@material-ui/icons/Edit';
 
 const columns: GridColDef[] = [
     {field: "id", headerName: "Familie ID", type: "number", width: 200}, 
@@ -27,8 +33,12 @@ interface IFamilyDialog{
  
 const FamilyDialog: React.FC<IFamilyDialog> = ({ open, accessToken,institution, handleClose}) => {
 
-  const apiservice = new ApiService(accessToken);
-  const [recipientData, setRecipientData] = useState<RecipientType[] | []>([]);
+
+    const apiservice = new ApiService(accessToken);
+    const [recipientData, setRecipientData] = useState<RecipientType[] | []>([]);
+    const [familyIdInput, setFamilyIdInput] = useState("");
+    const [showEditDialog, setShowEditDialog] = useState(false);
+    const [editRecipient, setEditRecipient] = useState({} as RecipientType)
  
     const fetchRecipients = () =>{
         apiservice
@@ -39,28 +49,74 @@ const FamilyDialog: React.FC<IFamilyDialog> = ({ open, accessToken,institution, 
           console.error(errorStack)
         });
     }
-      useEffect(() => {
-          fetchRecipients();
-      }, []);
+
+    useEffect(() => {
+        fetchRecipients();
+    }, []);
+
+    const onFamilyInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFamilyIdInput(e.target.value);
+        
+        let matchingRecipient = recipientData.find(recipient => recipient.familyId === e.target.value);
+        if(matchingRecipient != undefined){
+            setEditRecipient(matchingRecipient);
+        } else {
+            setEditRecipient({} as RecipientType);
+        }
+    }
+
+    const onEditFamily = () => {
+        if(editRecipient["rowKey"]){
+            setShowEditDialog(true)
+        }
+    }
+
+    const refreshData = () => {
+        console.log("UPDATE RECIPIENTS BY FETCHING AGAIN")
+
+        // Make sure that this works
+        fetchRecipients();
+    }
     
     let rows = [];
     for(let i = 0; i<recipientData.length; i++){
         rows[i] = {id: recipientData[i].familyId, refId: recipientData[i].referenceId, contactName: recipientData[i].contactFullName, contactMail: recipientData[i].contactEmail, contactPhone: recipientData[i].contactPhoneNumber};
     }
+
     return (
          <div>
              <Dialog fullWidth={true} open={open} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
-                 <DialogTitle id="alert-dialog-title"> 
-                     {"Tidligere registrerte familier"}
-                 </DialogTitle>
-                 <DialogContent style={{height:'800px'}}>
-                    <DataGrid
-                     rows={rows}
-                     columns={columns}
-                     autoPageSize
-                     pagination
-                     />
-                 </DialogContent>
+                <DialogTitle id="alert-dialog-title"> 
+                    {"Tidligere registrerte familier"}
+                </DialogTitle>
+                <DialogContent style={{height:'800px'}}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    autoPageSize
+                    pagination
+                    />
+                </DialogContent>
+
+                <Box sx={{ ml: 5 }}>
+                    <TextField
+                        type="number"
+                        label="Family ID"
+                        value={familyIdInput}
+                        onChange={onFamilyInputChange}
+                        />
+                    <IconButton aria-label="expand row" size="small" 
+                        onClick={onEditFamily}>
+                        <EditIcon/>
+                    </IconButton>
+                </Box>
+                <EditFamilyDialog
+                    open={showEditDialog}
+                    onClose = {() => { setShowEditDialog(false) }}
+                    refreshRecipients={() => refreshData()}
+                    recipient={editRecipient}
+                />
+
                 <DialogActions>
                     <Button onClick={handleClose}>
                         Lukk vindu
