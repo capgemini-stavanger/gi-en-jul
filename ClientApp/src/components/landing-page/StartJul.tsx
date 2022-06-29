56f517141df2e46c557436d8517989ba74f5aa5d
hildmi <hildem90@gmail.com>
2022-06-29 11:52:49 +0200

    added onClick and styling

--- ClientApp/src/components/landing-page/StartJul.tsx
+++ ClientApp/src/components/landing-page/StartJul.tsx
@@ -12,30 +12,13 @@ const StartJul = () => {
       <NavBarPublic />
       <Container id="start" className={classes.root} maxWidth={false}>
         <div className={classes.headLineContainer}>
-<<<<<<< HEAD
           <Typography className={classes.textHeadline}>
-            Hvordan du starter Gi en Jul i din kommune
+            Hvordan du starter Gi en jul i din kommune
           </Typography>
-=======
-            <Typography className={classes.textHeadline}>
-                Hvordan du starter Gi en jul i din kommune 
-            </Typography>
->>>>>>> 5b8fccb (added onClick and styling)
         </div>
         <Grid container direction="column" justifyContent="center" alignItems="center">
           <Grid item>
-            <Typography className={classes.sectionContainer}>
-<<<<<<< HEAD
-<<<<<<< HEAD
-              For å starte Gi en Jul i din kommune kan du ta kontakt med kontaktpersonen for
-              Stavanger kommune.
-=======
-                 For å starte Gi en jul i din kommune kan du ta kontakt med kontaktpersonen for Stavanger kommune.  
->>>>>>> 5b8fccb (added onClick and styling)
-=======
-                 For å starte Gi en jul i din kommune kan du ta kontakt med kontaktpersonen for Stavanger kommune.  
->>>>>>> c9587fa (added onClick and styling)
-            </Typography>
+            <Typography className={classes.sectionContainer}></Typography>
             <img className={classes.familyImage} src={family}></img>
           </Grid>
           <Grid item>
