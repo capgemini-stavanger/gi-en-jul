92457a66e34bba5a7c9cd7fc98b07ddd2c26f719
hildmi <hildem90@gmail.com>
2022-07-05 12:22:21 +0200

    m

--- ClientApp/src/components/institution/FormWish.tsx
+++ ClientApp/src/components/institution/FormWish.tsx
@@ -1,4 +1,8 @@
 import { Grid, Typography } from "@material-ui/core";
+import InputValidator from "components/shared/input-fields/validators/InputValidator";
+import { isNotNull } from "components/shared/input-fields/validators/Validators";
+import { Categories, ICategories } from "./mockDatabase";
+import { useState } from "react";
 
 interface IFormWish {
   cat: string;
@@ -6,15 +10,52 @@ interface IFormWish {
 }
 
 interface IWishProps {
-  wish: IFormWish;
+  // wish: IFormWish;
+  viewErrorTrigger: number;
+  updateWish: (newWishData: { [target: string]: unknown }) => void;
 }
 
-const InstitutionWish = ({}) => {
+const initState: { [data: string]: any } = {
+  wishInput: "",
+  ageWish: false,
+};
+
+const InstitutionWish: React.FC<IWishProps> = ({ viewErrorTrigger, updateWish }) => {
+  const [state, setState] = useState({ ...initState });
+
+  const getSetter = (target: keyof typeof state) => (value: typeof state[typeof target]) => {
+    setState((prev) => {
+      prev[target] = value;
+      return prev;
+    });
+  };
+
+  const onWishInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newInput = e.target.value;
+    getSetter("wishInput")(newInput);
+    updateWish({ wish: newInput });
+  };
+
   return (
     <Grid>
-      <Grid item>
-        <Typography> Test</Typography>
-      </Grid>
+      {!state.ageWish && (
+        <Grid item>
+          <InputValidator
+            viewErrorTrigger={viewErrorTrigger}
+            validators={[isNotNull]}
+            name="wish"
+            type="select"
+            label="gaveønske"
+            options={Categories.map((o: ICategories) => {
+              return { value: o.type, text: o.type };
+            })}
+            disabled={state.ageWish}
+            value={state.wishInput}
+            onChange={onWishInputChange}
+            fullWidth
+          ></InputValidator>
+        </Grid>
+      )}
     </Grid>
   );
 };
