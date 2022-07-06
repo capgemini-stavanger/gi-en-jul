221188c9dc060b01ab5eae0ac63c01746ac132b7
Håvard Moe Jacobsen <havard.moe-jacobsen@capgemini.com>
2022-07-06 11:53:47 +0200

    Possible for superadmin to edit kommune info

--- ClientApp/src/components/superadmin/ManageDashboard.tsx
+++ ClientApp/src/components/superadmin/ManageDashboard.tsx
@@ -1,6 +1,5 @@
 import { Tab, Typography } from "@material-ui/core";
 import { TabContext, TabList, TabPanel } from "@material-ui/lab";
-// import ApiService from "common/functions/apiServiceClass";
 import useStyles from "components/superadmin/Styles";
 import React, { useState } from "react";
 import { Container } from "reactstrap";
@@ -13,7 +12,6 @@ interface IManageDashboard {
 const ManageDashboard: React.FC<IManageDashboard> = ({ accessToken }) => {
   const classes = useStyles();
   const [tab, setTab] = useState<string>("1");
-  // const apiservice = new ApiService(accessToken);
 
   const handleChange = (event: React.ChangeEvent<any>, newValue: string) => {
     setTab(newValue);
