ac3f4dcbac45774677e97b3f4adfe03f51536cf2
havardMoe <54802685+havardMoe@users.noreply.github.com>
2022-07-08 09:11:59 +0200

    /kommune working communication with backend (#749)
    * working communication with backend
    
    * filenameChange
    
    Co-authored-by: Håvard Moe Jacobsen <havard.moe-jacobsen@capgemini.com>

--- ClientApp/src/pages/municipality/index.tsx
+++ ClientApp/src/pages/municipality/index.tsx
@@ -4,20 +4,70 @@ import useStyles from "components/landing-page/Styles";
 import logo from "styling/img/logo_background.svg";
 import family from "styling/img/familyTop.svg";
 import snowDown from "styling/img/snow_down.svg";
-import { CityData } from "components/municipalities/City";
+import { LocationData } from "components/municipalities/Kommune";
 import Footer from "components/shared/Footer";
 import { useState, useEffect } from "react";
 import ApiService from "common/functions/apiServiceClass";
 import NavBarPublic from "components/shared/navbar/NavBarPublic";
-import City from "components/municipalities/City";
+import Kommune from "components/municipalities/Kommune";
+
+interface IKommuneInfoResponse {
+  rowKey: string;
+  info: string;
+}
 
 const Municipality = () => {
-  const [cities, setMunicipalities] = useState<CityData[]>([]);
+  const [activeLocations, setActiveLocations] = useState<string[]>([]);
+  const [locationInfos, setLocationInfos] = useState(new Map<string, string>());
+  const updateMap = (location: string, info: string) => {
+    setLocationInfos(new Map(locationInfos.set(location, info)));
+  };
+  const [locationData, setLocationData] = useState<LocationData[]>([]);
+
   const classes = useStyles();
   const apiservice = new ApiService();
-  useEffect(() => {
-    apiservice.get("event/contacts").then((response) => setMunicipalities(response.data));
-  }, []);
+
+  const fetchActiveLocations = () => {
+    apiservice
+      .get("Event/ActiveLocations", {})
+      .then((resp) => {
+        setActiveLocations(resp.data);
+      })
+      .catch((errorStack) => {
+        console.error(errorStack);
+      });
+  };
+
+  const fetchKommuneInformation = () => {
+    apiservice
+      .get("Cms/getall", { params: { contentType: "Kommune" } })
+      .then((resp) => {
+        // Updates a dictionary with {location: information} pairs
+        resp.data.forEach((obj: IKommuneInfoResponse) => {
+          updateMap(obj.rowKey, obj.info);
+        });
+      })
+      .catch((errorStack) => {
+        console.error(errorStack);
+      });
+  };
+
+  const buildLocationData = () => {
+    const tempLocationData: LocationData[] = [];
+
+    activeLocations.forEach((location: string) => {
+      let info = locationInfos.get(location);
+      if (info === undefined) {
+        info = "Ingen informasjon om denne kommunen enda";
+      }
+      tempLocationData.push({ location: location, information: info });
+      setLocationData(tempLocationData);
+    });
+  };
+
+  useEffect(fetchActiveLocations, []);
+  useEffect(fetchKommuneInformation, []);
+  useEffect(buildLocationData, [activeLocations, locationInfos]);
 
   return (
     <>
@@ -34,7 +84,7 @@ const Municipality = () => {
             <img className={classes.snowDown} src={snowDown}></img>
           </Grid>
         </Grid>
-        <City cities={cities} />
+        <Kommune locations={locationData} />
         <ScrollToTop maxPagePosition={300} />
         <Footer />
       </Container>
