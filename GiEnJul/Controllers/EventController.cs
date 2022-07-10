fc8f74a8ef2e12df85d29fa34e388b1e04c0a321
Håvard Moe Jacobsen <havard.moe-jacobsen@capgemini.com>
2022-07-10 10:47:44 +0200

    Backend for fetching all events

--- GiEnJul/Controllers/EventController.cs
+++ GiEnJul/Controllers/EventController.cs
@@ -2,9 +2,11 @@
 using GiEnJul.Dtos;
 using GiEnJul.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using Serilog;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using GiEnJul.Auth;
 
 namespace GiEnJul.Controllers
 {
@@ -28,6 +30,14 @@ namespace GiEnJul.Controllers
         public async Task<string[]> GetActiveLocationsAsync()
         {
             return await _eventRepository.GetLocationsWithActiveEventAsync();
+        }
+
+        // GET api/Event/GetAll
+        [HttpGet("GetAll")]
+        [Authorize(Policy = Policy.SuperAdmin)]
+        public async Task<List<Models.Event>> GetAllEvents()
+        {
+            return await _eventRepository.GetAllEventsAsync();
         }
 
         [HttpGet("contacts")]
