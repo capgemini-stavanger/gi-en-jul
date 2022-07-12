55b60df92b1056c31238c1c791828d2aa157d6f1
hildmi <hildem90@gmail.com>
2022-07-12 10:39:03 +0200

    added logo to navbar, dynamic positioning

--- ClientApp/src/components/shared/navbar/NavBarPublic.tsx
+++ ClientApp/src/components/shared/navbar/NavBarPublic.tsx
@@ -1,115 +1,93 @@
-import {
-  Button,
-  IconButton,
-  Toolbar,
-  Drawer,
-  List,
-  ListItem,
-  Typography,
-  Tab,
-  Box,
-  Grid,
-} from "@material-ui/core";
+import { Button, IconButton, Tab, Box, Grid } from "@material-ui/core";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { Link as Scroll } from "react-scroll";
+
 import logo from "styling/img/logo_green.svg";
 import useStyles from "components/shared/Styles";
-import MenuIcon from "@material-ui/icons/Menu";
-import { ArrowForwardIos, Close } from "@material-ui/icons";
+
+import { ArrowForwardIos } from "@material-ui/icons";
 import { useHistory } from "react-router-dom";
 import { Navbar, NavbarBrand } from "reactstrap";
 import { TabContext, TabList } from "@material-ui/lab";
 
 const NavBarPublic = () => {
-  const [tab, setTab] = useState<string>("1");
+  const [tab] = useState<string>("1");
   const classes = useStyles();
   const history = useHistory();
 
-  const handleChange = (event: React.ChangeEvent<any>, newValue: string) => {
-    setTab(newValue);
-  };
-
-  if (window.location.pathname === "/bli-giver") {
-    return (
-      <>
-        <Navbar className={classes.navContainer}>
-          <Toolbar className={classes.toolBar}>
-            <IconButton>
-              <Link to="/">
-                <img className={classes.smallLogo} src={logo}></img>
-              </Link>
-            </IconButton>
-          </Toolbar>
-        </Navbar>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Navbar className={classes.navContainer}>
-          <NavbarBrand className={classes.toolBar}>
-            <TabContext value={tab}>
-              <TabList onChange={handleChange} centered className={classes.toolBar}>
-                <Grid container justifyContent="center" direction="row" spacing={10}>
-                  <Grid item>
-                    <Tab
-                      onClick={() => {
-                        history.push("/");
-                      }}
-                      className={classes.drawerContent}
-                      label="Hjem"
-                    ></Tab>
-                  </Grid>
-
-                  <Grid item>
-                    <Tab
-                      onClick={() => {
-                        history.push("/bedrift");
-                      }}
-                      className={classes.drawerContent}
-                      label="For bedrifter"
-                    ></Tab>
-                  </Grid>
-
-                  <Grid item>
-                    <Tab
-                      onClick={() => {
-                        history.push("/startJul");
-                      }}
-                      className={classes.drawerContent}
-                      label="Start Gi en jul i din kommune"
-                    ></Tab>
-                  </Grid>
-
-                  <Grid item>
-                    <Tab
-                      onClick={() => {
-                        history.push("kommune");
-                      }}
-                      className={classes.drawerContent}
-                      label="Kommuner"
-                    ></Tab>
-                  </Grid>
+  return (
+    <>
+      <Navbar className={classes.navContainer}>
+        <NavbarBrand className={classes.toolBar}>
+          <TabContext value={tab}>
+            <TabList centered className={classes.toolBar}>
+              <Grid container justifyContent="center" direction="row" spacing={10}>
+                <Grid item>
+                  <IconButton
+                    onClick={() => {
+                      history.push("/");
+                    }}
+                  >
+                    <img className={classes.smallLogo} src={logo}></img>
+                  </IconButton>
                 </Grid>
-              </TabList>
-            </TabContext>
-          </NavbarBrand>
-        </Navbar>
-        <Grid item>
-          <Box alignItems={"right"}>
-            <Button
-              size="large"
-              className={classes.giverButton}
-              endIcon={<ArrowForwardIos />}
-              onClick={React.useCallback(() => history.push("/bli-giver"), [history])}
-            >
-              Bli giver
-            </Button>
-          </Box>
-        </Grid>
-      </>
-    );
-  }
+
+                <Grid item>
+                  <Tab
+                    onClick={() => {
+                      history.push("/");
+                    }}
+                    className={classes.drawerContent}
+                    label="Hjem"
+                  ></Tab>
+                </Grid>
+
+                <Grid item>
+                  <Tab
+                    onClick={() => {
+                      history.push("/bedrift");
+                    }}
+                    className={classes.drawerContent}
+                    label="For bedrifter"
+                  ></Tab>
+                </Grid>
+
+                <Grid item>
+                  <Tab
+                    onClick={() => {
+                      history.push("/startJul");
+                    }}
+                    className={classes.drawerContent}
+                    label="Start Gi en jul i din kommune"
+                  ></Tab>
+                </Grid>
+
+                <Grid item>
+                  <Tab
+                    onClick={() => {
+                      history.push("kommune");
+                    }}
+                    className={classes.drawerContent}
+                    label="Kommuner"
+                  ></Tab>
+                </Grid>
+              </Grid>
+            </TabList>
+          </TabContext>
+        </NavbarBrand>
+      </Navbar>
+      <Grid item>
+        <Box alignItems={"right"}>
+          <Button
+            size="large"
+            className={classes.giverButton}
+            endIcon={<ArrowForwardIos />}
+            onClick={React.useCallback(() => history.push("/bli-giver"), [history])}
+          >
+            Bli giver
+          </Button>
+        </Box>
+      </Grid>
+    </>
+  );
 };
 export default NavBarPublic;
