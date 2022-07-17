c82f3d898472f1e3e3a280584731701a8916221f
RonHaland <ronny.haland@gmail.com>
2022-07-17 11:39:10 +0200

    FIx person -> submittedPersonExcel mapping

--- GiEnJul/Infrastructure/ExcelProfile.cs
+++ GiEnJul/Infrastructure/ExcelProfile.cs
@@ -12,7 +12,9 @@ namespace GiEnJul.Infrastructure
             CreateMap<Models.Recipient, Utilities.ExcelClasses.SubmittedFamiliesExcel>();
 
             CreateMap<Models.Person, Utilities.ExcelClasses.SubmittedPersonExcel>()
-                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.Age > 0 ? $"{src.Age}" : $"{src.Months} mnd"));
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.Age > 0 ? $"{src.Age}" : $"{src.Months} mnd"))
+                .ForMember(dest => dest.FamilyId, opt => opt.Ignore())
+                .ForMember(dest => dest.ReferenceId, opt => opt.Ignore());
         }
     }
 }
