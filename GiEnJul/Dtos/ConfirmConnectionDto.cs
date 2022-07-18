efa292b3cec4596c7c39829629b39105857809f1
RonHaland <ronny.haland@gmail.com>
2022-07-18 08:43:21 +0200

    admin can confirm connections on behalf of giver (#811)

---
+++ GiEnJul/Dtos/ConfirmConnectionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GiEnJul.Dtos
+{
+    public class ConfirmConnectionDto
+    {
+        public string GiverId { get; set; }
+        public string RecipientId { get; set; }
+        public string Event { get; set; }
+    }
+}
