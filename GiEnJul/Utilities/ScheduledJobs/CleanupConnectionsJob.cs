3e56fb0122c5d229e27303956ef1fabf65097844
RonHaland <ronny.haland@gmail.com>
2022-07-18 09:49:31 +0200

    Add scheduled job functionality (#818)

---
+++ GiEnJul/Utilities/ScheduledJobs/CleanupConnectionsJob.cs
@@ -0,0 +1,26 @@
+﻿using GiEnJul.Repositories;
+using Quartz;
+using Serilog;
+using System.Threading.Tasks;
+
+namespace GiEnJul.Utilities.ScheduledJobs
+{
+    public class CleanupConnectionsJob : IJob
+    {
+        private readonly ILogger _log;
+        private readonly IGiverRepository _giverRepository;
+
+        public CleanupConnectionsJob(ILogger log, IGiverRepository giverRepository)
+        {
+            _log = log;
+            _giverRepository = giverRepository;
+        }
+        
+        public async Task Execute(IJobExecutionContext context)
+        {
+            _log.Information("Start Job");
+            await Task.Delay(150);
+            _log.Information("Stop Job");
+        }
+    }
+}
---
+++ GiEnJul/Utilities/ServiceCollectionQuartzConfiguratorExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Quartz;
+
+namespace GiEnJul.Utilities
+{
+    public static class ServiceCollectionQuartzConfiguratorExtensions
+    {
+        public static IServiceCollectionQuartzConfigurator AddScheduledJob<T>(
+        this IServiceCollectionQuartzConfigurator quartz,
+        IConfiguration config)
+        where T : IJob
+        {
+            var jobKey = new JobKey(typeof(T).Name);
+            var schedulerStatement = config.GetSection("Jobs")
+                                           .GetValue<string>(typeof(T).Name);
+
+            quartz.AddJob<T>(c => c.WithIdentity(jobKey));
+            quartz.AddTrigger(c => c.ForJob(jobKey)
+#if DEBUG
+                                    .StartNow() //Run job on immediatly in debug mode
+#else
+                                    .WithCronSchedule(schedulerStatement)
+#endif                              
+                                    );
+
+            return quartz;
+        }
+    }
+}
