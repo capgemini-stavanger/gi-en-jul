41c4abc81d09b9c79a6e1d75bbb072057532f96a
hildmi <hildem90@gmail.com>
2022-07-25 18:09:34 +0200

    get all users and display in table

--- ClientApp/src/components/superadmin/Users/AddUser.tsx
+++ ClientApp/src/components/superadmin/Users/AddUser.tsx
@@ -1,19 +1,58 @@
-import { Grid, Tab, Typography } from "@material-ui/core";
+import { Grid, Tab, TableBody, TableCell, TableRow, Typography } from "@material-ui/core";
 import { TabContext, TabList, TabPanel } from "@material-ui/lab";
-import React, { useState } from "react";
-import { Container } from "reactstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Table } from "reactstrap";
 import useStyles from "components/landing-page/Styles";
 import UserForm from "./UserForm";
 import ConfirmationBox from "components/shared/ConfirmationBox";
+import ApiService from "common/functions/apiServiceClass";
 
 interface IAddUser {
   accessToken: string;
 }
 
+export interface IUser {
+  email: string;
+  nickname: string;
+}
+const initInterfaceUser: IUser = {
+  email: "",
+  nickname: "",
+};
+
 const AddUser: React.FC<IAddUser> = ({ accessToken }) => {
   const classes = useStyles();
   const [tab, setTab] = useState<string>("1");
   const [confirmAdd, setConfirmAdd] = useState<boolean>(false);
+  const [emails, setEmails] = useState<string[]>([]);
+
+  const [nicknames, setNicknames] = useState<string[]>([]);
+
+  const apiservice = new ApiService(accessToken);
+
+  const fetchEmails = () => {
+    apiservice
+      .get("user/getallemails")
+      .then((resp) => {
+        setEmails(resp.data);
+      })
+      .catch((errorStack) => {
+        console.error(errorStack);
+      });
+  };
+  useEffect(fetchEmails, []);
+
+  const fetchNicknames = () => {
+    apiservice
+      .get("user/getallnicknames")
+      .then((resp) => {
+        setNicknames(resp.data);
+      })
+      .catch((errorStack) => {
+        console.error(errorStack);
+      });
+  };
+  useEffect(fetchNicknames, []);
 
   const handleChange = (event: React.ChangeEvent<any>, newValue: string) => {
     setTab(newValue);
@@ -22,10 +61,6 @@ const AddUser: React.FC<IAddUser> = ({ accessToken }) => {
   const handleConfirmAdd = (response: boolean) => {
     setConfirmAdd(response);
   };
-
-  const handleClose = () => {};
-
-  const handleConfirm = () => {};
 
   return (
     <Grid container spacing={3} direction="column">
@@ -65,10 +100,40 @@ const AddUser: React.FC<IAddUser> = ({ accessToken }) => {
           <ConfirmationBox
             open={confirmAdd}
             text={"Er du sikker på at du vil legge til brukeren?"}
-            handleClose={handleClose}
-            handleResponse={handleConfirm}
+            handleClose={() => setConfirmAdd(false)}
+            handleResponse={() => setConfirmAdd(confirmAdd)}
           ></ConfirmationBox>
         )}
+      </Grid>
+
+      <Grid container spacing={3} direction="column" alignItems="center">
+        <Grid item>
+          <Typography className={classes.textHeadline}>Alle brukere</Typography>
+        </Grid>
+
+        <Grid item>
+          <Table style={{ width: "800px" }}>
+            <TableBody>
+              <TableRow>
+                <TableCell>Navn</TableCell>
+                <TableCell>Email</TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell>
+                  {nicknames.map((nickname, index) => (
+                    <Typography key={index}>{nickname}</Typography>
+                  ))}
+                </TableCell>
+                <TableCell>
+                  {emails.map((email, index) => (
+                    <Typography key={index}>{email}</Typography>
+                  ))}
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Grid>
       </Grid>
     </Grid>
   );
