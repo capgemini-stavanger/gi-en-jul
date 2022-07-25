b94f9bc9ae8ffa26979386ab4cb654367f23e270
hildmi <hildem90@gmail.com>
2022-07-25 10:30:00 +0200

    try fix emailtemplates

--- GiEnJul.Test/UtilTests/EmailTemplateBuilderTests.cs
+++ GiEnJul.Test/UtilTests/EmailTemplateBuilderTests.cs
@@ -31,7 +31,7 @@ namespace GiEnJul.Test.UtilTests
                 { "recipientNote", string.Empty }
             };
             data.AddDictionary(ObjectToDictionaryHelper.MakeStringValueDict(new Giver(), "giver."));
-            data.AddDictionary(ObjectToDictionaryHelper.MakeStringValueDict(new Event(), "eventDto."));
+            data.AddDictionary(ObjectToDictionaryHelper.MakeStringValueDict(new Event(), "municipalityDto."));
             data.AddDictionary(ObjectToDictionaryHelper.MakeStringValueDict(new Recipient(), "recipient."));
 
             var result = await SUT.GetEmailTemplate(EmailTemplateName.AssignedFamily, data);
@@ -49,7 +49,7 @@ namespace GiEnJul.Test.UtilTests
                 { "familyRange", string.Empty }
             };
             data.AddDictionary(ObjectToDictionaryHelper.MakeStringValueDict(new Giver(), "giver."));
-            data.AddDictionary(ObjectToDictionaryHelper.MakeStringValueDict(new Event(), "eventDto."));
+            data.AddDictionary(ObjectToDictionaryHelper.MakeStringValueDict(new Event(), "municipalityDto."));
 
             var result = await SUT.GetEmailTemplate(EmailTemplateName.Registered, data);
             Assert.False(string.IsNullOrEmpty(result.Subject));
@@ -66,7 +66,7 @@ namespace GiEnJul.Test.UtilTests
                 { "familyRange", string.Empty }
             };
             data.AddDictionary(ObjectToDictionaryHelper.MakeStringValueDict(new Giver(), "giver."));
-            data.AddDictionary(ObjectToDictionaryHelper.MakeStringValueDict(new Event(), "eventDto."));
+            data.AddDictionary(ObjectToDictionaryHelper.MakeStringValueDict(new Event(), "municipalityDto."));
 
             var result = await SUT.GetEmailTemplate(EmailTemplateName.WaitingList, data);
             Assert.False(string.IsNullOrEmpty(result.Subject));
