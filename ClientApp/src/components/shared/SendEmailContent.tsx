51e4f8b24c6074347c4f8595b051851a311f41b1
Yohannes Dawit Kassaye <54357153+YohannesDK@users.noreply.github.com>
2022-08-02 15:20:57 +0200

    updated api path (#944)

--- ClientApp/src/components/shared/SendEmailContent.tsx
+++ ClientApp/src/components/shared/SendEmailContent.tsx
@@ -61,7 +61,7 @@ const SendEmailContent: React.FC<ISendSingleEmail> = ({ open, handleClose, toEma
     }
     await apiservice
       .post(
-        "email/send",
+        "email/sendFromUser",
         JSON.stringify({
           Subject: subjectInput,
           Content: html,
