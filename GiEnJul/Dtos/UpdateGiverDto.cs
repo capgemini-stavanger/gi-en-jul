1432af7479d2a098a7bb0ca75a563ccad045b16e
RonHaland <ronny.haland@gmail.com>
2022-11-25 14:56:33 +0100

    add way to edit giver email and phonenumber

---
+++ GiEnJul/Dtos/UpdateGiverDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GiEnJul.Dtos
+{
+    public class UpdateGiverDto
+    {
+        public Guid Id { get; set; }
+        public string Event { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
