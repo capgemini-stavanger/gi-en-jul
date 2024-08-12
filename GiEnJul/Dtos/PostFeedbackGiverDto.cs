fb677338ee66facb3886e50a57e880ef698ac831
RonHaland <ronnyhaland@gmail.com>
2024-08-12 12:50:46 +0200

    fix validation error when denying connection

--- GiEnJul/Dtos/PostFeedbackGiverDto.cs
+++ GiEnJul/Dtos/PostFeedbackGiverDto.cs
@@ -1,11 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace GiEnJul.Dtos
+﻿namespace GiEnJul.Dtos
 {
     public class PostFeedbackGiverDto
     {
         public bool DeleteGiver { get; set; }
-        [Required]
         public string FeedbackGiver { get; set; }
     }
 }
