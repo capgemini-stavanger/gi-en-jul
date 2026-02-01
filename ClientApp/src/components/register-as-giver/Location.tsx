70c890d7524cb5747a463867789568dbe7b337fb
ronhaland <ronnyhaland@gmail.com>
2026-02-01 11:27:25 +0100

    remove clg on form error

--- ClientApp/src/components/register-as-giver/Location.tsx
+++ ClientApp/src/components/register-as-giver/Location.tsx
@@ -50,7 +50,7 @@ const Location: React.FC<Props> = ({
         className={classes.form}
       >
         <Grid item>
-          <form onSubmit={nextStep} onError={(errors) => console.error(errors)}>
+          <form onSubmit={nextStep}>
             <InputValidator
               viewErrorTrigger={viewErrorTrigger}
               type="select"
